250b255be167a6818749e1e921a6629e7d2548e2
azyuen <azyuen@gmail.com>
2026-09-21 07:42:02 +1000

    R25 bump fast-load bundle

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
-import BootScene from './scenes/BootScene.js?v=20260921-r24';
-import GarageScene from './scenes/GarageScene.js?v=20260921-r24';
-import MeetScene from './scenes/MeetScene.js?v=20260921-r24';
-import RaceScene from './scenes/RaceScene.js?v=20260921-r24';
-import ResultScene from './scenes/ResultScene.js?v=20260921-r24';
+import BootScene from './scenes/BootScene.js?v=20260921-r25';
+import GarageScene from './scenes/GarageScene.js?v=20260921-r25';
+import MeetScene from './scenes/MeetScene.js?v=20260921-r25';
+import RaceScene from './scenes/RaceScene.js?v=20260921-r25';
+import ResultScene from './scenes/ResultScene.js?v=20260921-r25';
 
 const GAME_WIDTH = 1560;
 const GAME_HEIGHT = 720;
