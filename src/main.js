7c133b1eb2045a1c1955439b0c2400f8602578b2
azyuen <azyuen@gmail.com>
2026-09-20 22:52:04 +1000

    R7: refresh scene module cache keys

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
-import BootScene from './scenes/BootScene.js?v=20260920-r6';
-import GarageScene from './scenes/GarageScene.js?v=20260920-r6';
-import RaceScene from './scenes/RaceScene.js?v=20260920-r6';
-import ResultScene from './scenes/ResultScene.js?v=20260920-r6';
+import BootScene from './scenes/BootScene.js?v=20260920-r7';
+import GarageScene from './scenes/GarageScene.js?v=20260920-r7';
+import RaceScene from './scenes/RaceScene.js?v=20260920-r7';
+import ResultScene from './scenes/ResultScene.js?v=20260920-r7';
 
 const GAME_WIDTH = 1560;
 const GAME_HEIGHT = 720;
