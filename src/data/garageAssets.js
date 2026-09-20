df0a74953fdbdaa0c4d27357d856e4b0e460c0fc
azyuen <azyuen@gmail.com>
2026-09-21 00:07:57 +1000

    R14 use uploaded home and tuner workshop backgrounds

--- src/data/garageAssets.js
+++ src/data/garageAssets.js
@@ -1,5 +1,6 @@
 export const garageAssets = [
-  { key: 'garageWorkshopBg', path: 'assets/Garage/workshop_bg.svg' },
+  { key: 'garageWorkshopBg', path: 'assets/Garage/workshop_home.PNG' },
+  { key: 'garageWorkshopTunerBg', path: 'assets/Garage/Workshop_tuner.PNG' },
   { key: 'garageCharacterLeft', path: 'assets/Garage/character_left.svg' },
   { key: 'garageCharacterRight', path: 'assets/Garage/character_right.svg' },
 ];
