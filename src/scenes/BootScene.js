7de06421a6627a35edac545c8103ca934abeb684
azyuen <azyuen@gmail.com>
2026-09-20 22:52:01 +1000

    R7: update boot build marker

--- src/scenes/BootScene.js
+++ src/scenes/BootScene.js
@@ -30,7 +30,7 @@ export default class BootScene extends Phaser.Scene {
     this.add.text(780, 305, 'TOKYO SHIFT', {
       fontFamily: 'monospace', fontSize: '44px', color: '#e8f7ff', fontStyle: 'bold'
     }).setOrigin(0.5);
-    this.add.text(780, 363, 'R6 // GARAGE BUILD', {
+    this.add.text(780, 363, 'R7 // TOKYO BACKGROUND', {
       fontFamily: 'monospace', fontSize: '18px', color: '#62d8ff', letterSpacing: 2
     }).setOrigin(0.5);
 
