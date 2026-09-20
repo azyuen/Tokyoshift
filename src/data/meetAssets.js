8581a8b69b5b56659b9d99f79d5e5cc6e6e6163e
azyuen <azyuen@gmail.com>
2026-09-21 01:02:21 +1000

    R20 wire uploaded meet backgrounds

---
+++ src/data/meetAssets.js
@@ -0,0 +1,12 @@
+export const meetBackgrounds = [
+  {
+    key: 'meetWangan711',
+    path: 'assets/Meet/Backgrounds/Meet_wangan_711.PNG',
+    label: 'WANGAN // 7-ELEVEN',
+  },
+  {
+    key: 'meetWanganBridge',
+    path: 'assets/Meet/Backgrounds/Meet_wangan_bridge.PNG',
+    label: 'WANGAN // BRIDGE',
+  },
+];
