dc9f846fbc5590bcc8ce3206ebf878ca1f8b7c37
azyuen <azyuen@gmail.com>
2026-09-21 00:20:33 +1000

    R16 bump workshop bundle

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
-import BootScene from './scenes/BootScene.js?v=20260921-r15';
-import GarageScene from './scenes/GarageScene.js?v=20260921-r15';
-import RaceScene from './scenes/RaceScene.js?v=20260921-r15';
-import ResultScene from './scenes/ResultScene.js?v=20260921-r15';
+import BootScene from './scenes/BootScene.js?v=20260921-r16';
+import GarageScene from './scenes/GarageScene.js?v=20260921-r16';
+import RaceScene from './scenes/RaceScene.js?v=20260921-r16';
+import ResultScene from './scenes/ResultScene.js?v=20260921-r16';
 
 const GAME_WIDTH = 1560;
 const GAME_HEIGHT = 720;
