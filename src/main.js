cefcd84a48f238acedbfa2aa3e2ad21a378630d9
azyuen <azyuen@gmail.com>
2026-09-20 23:10:58 +1000

    R9: refresh race scene cache key

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
-import BootScene from './scenes/BootScene.js?v=20260920-r8';
-import GarageScene from './scenes/GarageScene.js?v=20260920-r8';
-import RaceScene from './scenes/RaceScene.js?v=20260920-r8';
-import ResultScene from './scenes/ResultScene.js?v=20260920-r8';
+import BootScene from './scenes/BootScene.js?v=20260920-r9';
+import GarageScene from './scenes/GarageScene.js?v=20260920-r9';
+import RaceScene from './scenes/RaceScene.js?v=20260920-r9';
+import ResultScene from './scenes/ResultScene.js?v=20260920-r9';
 
 const GAME_WIDTH = 1560;
 const GAME_HEIGHT = 720;
