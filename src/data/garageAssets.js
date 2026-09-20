a86da1135186cf386d9e940b32b05f0780929a84
azyuen <azyuen@gmail.com>
2026-09-20 23:26:51 +1000

    R10 add asset-driven workshop slots

---
+++ src/data/garageAssets.js
@@ -0,0 +1,5 @@
+export const garageAssets = [
+  { key: 'garageWorkshopBg', path: 'assets/Garage/workshop_bg.svg' },
+  { key: 'garageCharacterLeft', path: 'assets/Garage/character_left.svg' },
+  { key: 'garageCharacterRight', path: 'assets/Garage/character_right.svg' },
+];
