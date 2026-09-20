1bd7f863a228418ec8ceb2b754301e921ae945b8
azyuen <azyuen@gmail.com>
2026-09-21 09:14:39 +1000

    R29 preserve original full-screen race canvas

--- src/scenes/RaceScene.js
+++ src/scenes/RaceScene.js
@@ -1,12 +1,12 @@
 import Vehicle from '../vehicles/Vehicle.js';
 import TouchControls from '../input/TouchControls.js?v=20260920-r6';
-import DragRacingAI from '../ai/DragRacingAI.js?v=20260921-r27';
+import DragRacingAI from '../ai/DragRacingAI.js?v=20260921-r29';
 import RaceHUD from '../ui/RaceHUD.js?v=20260920-r6';
 import DebugHUD from '../ui/DebugHUD.js';
 import TokyoExpresswayBackground from '../environment/TokyoExpresswayBackground.js?v=20260920-r7';
-import { cars, carOrder } from '../data/cars.js?v=20260921-r27';
-import { engines } from '../data/engines.js?v=20260921-r27';
-import { characters } from '../data/characters.js?v=20260921-r27';
+import { cars, carOrder } from '../data/cars.js?v=20260921-r29';
+import { engines } from '../data/engines.js?v=20260921-r29';
+import { characters } from '../data/characters.js?v=20260921-r29';
 
 const TRACK_M = 402.336;
 const PX_PER_M = 76.0;
@@ -49,6 +49,7 @@ export default class RaceScene extends Phaser.Scene {
 
   create() {
     document.body.dataset.scene = 'race';
+    this.scale.resize(1560, 720);
 
     const playerConfig = clone(cars[this.selectedCarId]);
     const opponentConfig = clone(cars[this.opponentCarId]);
