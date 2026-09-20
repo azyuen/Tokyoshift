a3a72603f094a584b168ab9d04e91278c95d45de
azyuen <azyuen@gmail.com>
2026-09-21 00:15:18 +1000

    R15 cache-bust uploaded workshop artwork

--- src/data/garageAssets.js
+++ src/data/garageAssets.js
@@ -1,6 +1,6 @@
 export const garageAssets = [
-  { key: 'garageWorkshopBg', path: 'assets/Garage/workshop_home.PNG' },
-  { key: 'garageWorkshopTunerBg', path: 'assets/Garage/Workshop_tuner.PNG' },
+  { key: 'garageWorkshopBg', path: 'assets/Garage/workshop_home.PNG?v=20260921-r15' },
+  { key: 'garageWorkshopTunerBg', path: 'assets/Garage/Workshop_tuner.PNG?v=20260921-r15' },
   { key: 'garageCharacterLeft', path: 'assets/Garage/character_left.svg' },
   { key: 'garageCharacterRight', path: 'assets/Garage/character_right.svg' },
 ];
