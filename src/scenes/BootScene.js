48ace0f5e19f73a755b45b6869c980d916268bad
azyuen <azyuen@gmail.com>
2026-09-21 00:47:49 +1000

    R18 bump boot bundle

--- src/scenes/BootScene.js
+++ src/scenes/BootScene.js
@@ -1,5 +1,5 @@
-import { garageAssets } from '../data/garageAssets.js?v=20260921-r17';
-import { characters } from '../data/characters.js?v=20260921-r17';
+import { garageAssets } from '../data/garageAssets.js?v=20260921-r18';
+import { characters } from '../data/characters.js?v=20260921-r18';
 
 export default class BootScene extends Phaser.Scene {
   constructor() { super('BootScene'); }
@@ -33,7 +33,7 @@ export default class BootScene extends Phaser.Scene {
     workshopCharacters.forEach(character => {
       this.load.image(
         character.visual.spriteKey,
-        character.visual.path + '?v=20260921-r17'
+        character.visual.path + '?v=20260921-r18'
       );
     });
   }
@@ -57,7 +57,7 @@ export default class BootScene extends Phaser.Scene {
     this.add.text(780, 304, 'TOKYO SHIFT', {
       fontFamily: '"Silkscreen", monospace', fontSize: '40px', color: '#e8f7ff'
     }).setOrigin(0.5);
-    this.add.text(780, 363, 'R17 // REN + DAICHI', {
+    this.add.text(780, 363, 'R18 // NIGHT MEET', {
       fontFamily: '"Silkscreen", monospace', fontSize: '16px', color: '#62d8ff'
     }).setOrigin(0.5);
 
