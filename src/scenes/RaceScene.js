c009a14a1c039a0870c1efa20a828e6d6c0d627f
azyuen <azyuen@gmail.com>
2026-09-21 08:15:25 +1000

    R26 bump race bundle

--- src/scenes/RaceScene.js
+++ src/scenes/RaceScene.js
@@ -1,12 +1,12 @@
 import Vehicle from '../vehicles/Vehicle.js';
 import TouchControls from '../input/TouchControls.js?v=20260920-r6';
-import DragRacingAI from '../ai/DragRacingAI.js?v=20260921-r25';
+import DragRacingAI from '../ai/DragRacingAI.js?v=20260921-r26';
 import RaceHUD from '../ui/RaceHUD.js?v=20260920-r6';
 import DebugHUD from '../ui/DebugHUD.js';
 import TokyoExpresswayBackground from '../environment/TokyoExpresswayBackground.js?v=20260920-r7';
-import { cars, carOrder } from '../data/cars.js?v=20260921-r25';
-import { engines } from '../data/engines.js?v=20260921-r25';
-import { characters } from '../data/characters.js?v=20260921-r25';
+import { cars, carOrder } from '../data/cars.js?v=20260921-r26';
+import { engines } from '../data/engines.js?v=20260921-r26';
+import { characters } from '../data/characters.js?v=20260921-r26';
 
 const TRACK_M = 402.336;
 const PX_PER_M = 76.0;
