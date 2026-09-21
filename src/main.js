7c504bbcfad5fe0ce7b642d3224b951ba843dcd5
azyuen <azyuen@gmail.com>
2026-09-21 18:25:24 +1000

    R61: bump scene module versions

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
-import BootScene from './scenes/BootScene.js?v=20260921-r60';
-import CharacterSelectScene from './scenes/CharacterSelectScene.js?v=20260921-r60';
-import GarageScene from './scenes/GarageScene.js?v=20260921-r60';
-import MeetScene from './scenes/MeetScene.js?v=20260921-r60';
-import RaceScene from './scenes/RaceScene.js?v=20260921-r60';
-import ResultScene from './scenes/ResultScene.js?v=20260921-r60';
+import BootScene from './scenes/BootScene.js?v=20260921-r61';
+import CharacterSelectScene from './scenes/CharacterSelectScene.js?v=20260921-r61';
+import GarageScene from './scenes/GarageScene.js?v=20260921-r61';
+import MeetScene from './scenes/MeetScene.js?v=20260921-r61';
+import RaceScene from './scenes/RaceScene.js?v=20260921-r61';
+import ResultScene from './scenes/ResultScene.js?v=20260921-r61';
 
 
 // Phone readability pass.
