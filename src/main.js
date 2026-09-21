ab1fafbc7d48d4829bcdf6f7597e6818882adec5
azyuen <azyuen@gmail.com>
2026-09-21 15:47:26 +1000

    Load travel map and day races in R49

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
-import BootScene from './scenes/BootScene.js?v=20260921-r48';
-import CharacterSelectScene from './scenes/CharacterSelectScene.js?v=20260921-r48';
-import GarageScene from './scenes/GarageScene.js?v=20260921-r48';
-import MeetScene from './scenes/MeetScene.js?v=20260921-r48';
-import RaceScene from './scenes/RaceScene.js?v=20260921-r48';
-import ResultScene from './scenes/ResultScene.js?v=20260921-r48';
+import BootScene from './scenes/BootScene.js?v=20260921-r49';
+import CharacterSelectScene from './scenes/CharacterSelectScene.js?v=20260921-r49';
+import GarageScene from './scenes/GarageScene.js?v=20260921-r49';
+import MeetScene from './scenes/MeetScene.js?v=20260921-r49';
+import RaceScene from './scenes/RaceScene.js?v=20260921-r49';
+import ResultScene from './scenes/ResultScene.js?v=20260921-r49';
 
 
 // Phone readability pass.
