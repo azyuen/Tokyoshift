6baba94bcdd440d29cbabe52f5b55fe7d86462d2
azyuen <azyuen@gmail.com>
2026-09-21 20:34:13 +1000

    Load R65 tuning race and orientation updates

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
-import BootScene from './scenes/BootScene.js?v=20260921-r64';
-import CharacterSelectScene from './scenes/CharacterSelectScene.js?v=20260921-r64';
-import GarageScene from './scenes/GarageScene.js?v=20260921-r64';
-import MeetScene from './scenes/MeetScene.js?v=20260921-r64';
-import RaceScene from './scenes/RaceScene.js?v=20260921-r64';
-import ResultScene from './scenes/ResultScene.js?v=20260921-r64';
+import BootScene from './scenes/BootScene.js?v=20260921-r65';
+import CharacterSelectScene from './scenes/CharacterSelectScene.js?v=20260921-r65';
+import GarageScene from './scenes/GarageScene.js?v=20260921-r65';
+import MeetScene from './scenes/MeetScene.js?v=20260921-r65';
+import RaceScene from './scenes/RaceScene.js?v=20260921-r65';
+import ResultScene from './scenes/ResultScene.js?v=20260921-r65';
 
 
 // Phone readability pass.
