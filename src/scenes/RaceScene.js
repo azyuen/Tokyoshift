6e0f60dfef26cd1c1c5ea4ee138d686fde3e8dca
azyuen <azyuen@gmail.com>
2026-09-21 16:13:49 +1000

    R52: drive engine audio from race telemetry

--- src/scenes/RaceScene.js
+++ src/scenes/RaceScene.js
@@ -9,7 +9,7 @@ import { engines } from '../data/engines.js?v=20260921-r43';
 import { characters } from '../data/characters.js?v=20260921-r43';
 import { WORKSHOP_RETURN_COST } from '../data/meetAssets.js?v=20260921-r50';
 import { saveSessionState, saveManualState, restoreManualSave, readManualSave, clearAllSaves } from '../state/GameState.js?v=20260921-r49';
-import { playRaceMusic, playVictorySting, stopMusic } from '../audio/MusicManager.js?v=20260921-r44';
+import { playRaceMusic, playVictorySting, stopMusic } from '../audio/MusicManager.js?v=20260921-r44';\nimport EngineAudioSystem from '../audio/EngineAudioSystem.js?v=20260921-r52';
 
 const TRACK_M = 402.336;
 const PX_PER_M = 76.0;
