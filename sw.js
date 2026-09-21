05cb7bcf18d171c5e71b1791b94b058b0de66619
azyuen <azyuen@gmail.com>
2026-09-21 18:25:27 +1000

    R61: refresh PWA caches for GPS map

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-const BUILD = 'R60';
-const ASSET_CACHE = 'tokyoshift-assets-v19';
-const RUNTIME_CACHE = 'tokyoshift-runtime-v19';
+const BUILD = 'R61';
+const ASSET_CACHE = 'tokyoshift-assets-v20';
+const RUNTIME_CACHE = 'tokyoshift-runtime-v20';
 
 self.addEventListener('install', () => self.skipWaiting());
 
