41e28ca20159fc6aae491a2121244f101ead027e
azyuen <azyuen@gmail.com>
2026-09-22 00:12:42 +1000

    R81: load sound fix build

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
-import BootScene from './scenes/BootScene.js?v=20260921-r80';
-import CharacterSelectScene from './scenes/CharacterSelectScene.js?v=20260921-r80';
-import GarageScene from './scenes/GarageScene.js?v=20260921-r80';
-import MeetScene from './scenes/MeetScene.js?v=20260921-r80';
-import RaceScene from './scenes/RaceScene.js?v=20260921-r80';
-import ResultScene from './scenes/ResultScene.js?v=20260921-r80';
+import BootScene from './scenes/BootScene.js?v=20260921-r81';
+import CharacterSelectScene from './scenes/CharacterSelectScene.js?v=20260921-r81';
+import GarageScene from './scenes/GarageScene.js?v=20260921-r81';
+import MeetScene from './scenes/MeetScene.js?v=20260921-r81';
+import RaceScene from './scenes/RaceScene.js?v=20260921-r81';
+import ResultScene from './scenes/ResultScene.js?v=20260921-r81';
 
 
 // Phone readability pass.
