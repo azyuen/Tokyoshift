03664139c2a85a784898bf27f91074d73dfba25b
azyuen <azyuen@gmail.com>
2026-09-21 16:57:34 +1000

    Unify game state module on R56

--- src/scenes/CharacterSelectScene.js
+++ src/scenes/CharacterSelectScene.js
@@ -1,6 +1,6 @@
 import { cars } from '../data/cars.js?v=20260921-r43';
 import { characters, characterOrder } from '../data/characters.js?v=20260921-r43';
-import { createDefaultGameState, applyStateToRegistry, saveSessionState } from '../state/GameState.js?v=20260921-r55';
+import { createDefaultGameState, applyStateToRegistry, saveSessionState } from '../state/GameState.js?v=20260921-r56';
 import { playMusic } from '../audio/MusicManager.js?v=20260921-r56';
 
 const PIXEL_FONT = '"Silkscreen", monospace';
