67aa3a5294513ddca2d4542a9cc9f8df626c99ac
azyuen <azyuen@gmail.com>
2026-09-21 16:55:52 +1000

    Unify soundtrack module on R56

--- src/scenes/EngineTuningScene.js
+++ src/scenes/EngineTuningScene.js
@@ -12,7 +12,7 @@ import {
   applyEngineTuning,
 } from '../data/tuning.js?v=20260921-r55';
 import { saveSessionState } from '../state/GameState.js?v=20260921-r55';
-import { playMusic } from '../audio/MusicManager.js?v=20260921-r44';
+import { playMusic } from '../audio/MusicManager.js?v=20260921-r56';
 
 const PIXEL_FONT = '"Silkscreen", monospace';
 const BODY_FONT = '"Rajdhani", monospace';
