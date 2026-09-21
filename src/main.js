7a8bfaea0d634d451df619230ea162ec5b685eba
azyuen <azyuen@gmail.com>
2026-09-21 20:08:15 +1000

    R64: refresh scene modules

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
-import BootScene from './scenes/BootScene.js?v=20260921-r63';
-import CharacterSelectScene from './scenes/CharacterSelectScene.js?v=20260921-r63';
-import GarageScene from './scenes/GarageScene.js?v=20260921-r63';
-import MeetScene from './scenes/MeetScene.js?v=20260921-r63';
-import RaceScene from './scenes/RaceScene.js?v=20260921-r63';
-import ResultScene from './scenes/ResultScene.js?v=20260921-r63';
+import BootScene from './scenes/BootScene.js?v=20260921-r64';
+import CharacterSelectScene from './scenes/CharacterSelectScene.js?v=20260921-r64';
+import GarageScene from './scenes/GarageScene.js?v=20260921-r64';
+import MeetScene from './scenes/MeetScene.js?v=20260921-r64';
+import RaceScene from './scenes/RaceScene.js?v=20260921-r64';
+import ResultScene from './scenes/ResultScene.js?v=20260921-r64';
 
 
 // Phone readability pass.
