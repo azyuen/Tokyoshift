bcd7ebfc57e2388207509fbfd64b76c79b7809f8
azyuen <azyuen@gmail.com>
2026-09-21 11:38:32 +1000

    R35 bump scene modules

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
-import BootScene from './scenes/BootScene.js?v=20260921-r34';
-import GarageScene from './scenes/GarageScene.js?v=20260921-r34';
-import MeetScene from './scenes/MeetScene.js?v=20260921-r34';
-import RaceScene from './scenes/RaceScene.js?v=20260921-r34';
-import ResultScene from './scenes/ResultScene.js?v=20260921-r34';
+import BootScene from './scenes/BootScene.js?v=20260921-r35';
+import GarageScene from './scenes/GarageScene.js?v=20260921-r35';
+import MeetScene from './scenes/MeetScene.js?v=20260921-r35';
+import RaceScene from './scenes/RaceScene.js?v=20260921-r35';
+import ResultScene from './scenes/ResultScene.js?v=20260921-r35';
 
 
 // Phone readability pass.
