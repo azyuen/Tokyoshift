e4eee4b98b06ff68855d36f650d4bf213560aa53
azyuen <azyuen@gmail.com>
2026-09-21 15:46:45 +1000

    Refresh game state module for R49

--- src/scenes/BootScene.js
+++ src/scenes/BootScene.js
@@ -1,6 +1,6 @@
 import { garageAssets } from '../data/garageAssets.js?v=20260921-r43';
 import { characters, characterOrder } from '../data/characters.js?v=20260921-r43';
-import { createDefaultGameState, readManualSave, applyStateToRegistry } from '../state/GameState.js?v=20260921-r47';
+import { createDefaultGameState, readManualSave, applyStateToRegistry } from '../state/GameState.js?v=20260921-r49';
 
 export default class BootScene extends Phaser.Scene {
   constructor() { super('BootScene'); }
