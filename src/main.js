9b784f1998a66df35fb1a14ace8f0a1ef229c3a8
azyuen <azyuen@gmail.com>
2026-09-21 23:00:38 +1000

    R75: refresh game scenes

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
-import BootScene from './scenes/BootScene.js?v=20260921-r74';
-import CharacterSelectScene from './scenes/CharacterSelectScene.js?v=20260921-r74';
-import GarageScene from './scenes/GarageScene.js?v=20260921-r74';
-import MeetScene from './scenes/MeetScene.js?v=20260921-r74';
-import RaceScene from './scenes/RaceScene.js?v=20260921-r74';
-import ResultScene from './scenes/ResultScene.js?v=20260921-r74';
+import BootScene from './scenes/BootScene.js?v=20260921-r75';
+import CharacterSelectScene from './scenes/CharacterSelectScene.js?v=20260921-r75';
+import GarageScene from './scenes/GarageScene.js?v=20260921-r75';
+import MeetScene from './scenes/MeetScene.js?v=20260921-r75';
+import RaceScene from './scenes/RaceScene.js?v=20260921-r75';
+import ResultScene from './scenes/ResultScene.js?v=20260921-r75';
 
 
 // Phone readability pass.
