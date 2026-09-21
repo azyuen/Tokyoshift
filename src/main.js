8ce13366c68a965f256185774357354dcf0ed22f
azyuen <azyuen@gmail.com>
2026-09-22 00:18:47 +1000

    R82 load unified tuning headers

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
-import BootScene from './scenes/BootScene.js?v=20260921-r81';
-import CharacterSelectScene from './scenes/CharacterSelectScene.js?v=20260921-r81';
-import GarageScene from './scenes/GarageScene.js?v=20260921-r81';
-import MeetScene from './scenes/MeetScene.js?v=20260921-r81';
-import RaceScene from './scenes/RaceScene.js?v=20260921-r81';
-import ResultScene from './scenes/ResultScene.js?v=20260921-r81';
+import BootScene from './scenes/BootScene.js?v=20260921-r82';
+import CharacterSelectScene from './scenes/CharacterSelectScene.js?v=20260921-r82';
+import GarageScene from './scenes/GarageScene.js?v=20260921-r82';
+import MeetScene from './scenes/MeetScene.js?v=20260921-r82';
+import RaceScene from './scenes/RaceScene.js?v=20260921-r82';
+import ResultScene from './scenes/ResultScene.js?v=20260921-r82';
 
 
 // Phone readability pass.
