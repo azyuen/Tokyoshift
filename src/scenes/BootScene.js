6c45834034967c03475d310ecd0bee12380d6067
azyuen <azyuen@gmail.com>
2026-09-21 13:18:48 +1000

    R38 bump boot build

--- src/scenes/BootScene.js
+++ src/scenes/BootScene.js
@@ -1,5 +1,5 @@
-import { garageAssets } from '../data/garageAssets.js?v=20260921-r37';
-import { characters } from '../data/characters.js?v=20260921-r37';
+import { garageAssets } from '../data/garageAssets.js?v=20260921-r38';
+import { characters } from '../data/characters.js?v=20260921-r38';
 
 export default class BootScene extends Phaser.Scene {
   constructor() { super('BootScene'); }
@@ -29,7 +29,7 @@ export default class BootScene extends Phaser.Scene {
     workshopCharacters.forEach(character => {
       this.load.image(
         character.visual.spriteKey,
-        character.visual.path + '?v=20260921-r37'
+        character.visual.path + '?v=20260921-r38'
       );
     });
   }
@@ -56,7 +56,7 @@ export default class BootScene extends Phaser.Scene {
     this.add.text(780, 356, 'TOKYO SHIFT', {
       fontFamily: '"Silkscreen", monospace', fontSize: '40px', color: '#e8f7ff'
     }).setOrigin(0.5);
-    this.add.text(780, 425, 'R37 // FINAL COSMETICS', {
+    this.add.text(780, 425, 'R38 // GROUNDING SHADOWS', {
       fontFamily: '"Silkscreen", monospace', fontSize: '16px', color: '#62d8ff'
     }).setOrigin(0.5);
 
