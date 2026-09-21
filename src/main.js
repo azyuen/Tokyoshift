3f52c00bacb51711eac9179797ee9c4c50b1ce6a
azyuen <azyuen@gmail.com>
2026-09-21 15:56:36 +1000

    Load travel economy updates in R50

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
-import BootScene from './scenes/BootScene.js?v=20260921-r49';
-import CharacterSelectScene from './scenes/CharacterSelectScene.js?v=20260921-r49';
-import GarageScene from './scenes/GarageScene.js?v=20260921-r49';
-import MeetScene from './scenes/MeetScene.js?v=20260921-r49';
-import RaceScene from './scenes/RaceScene.js?v=20260921-r49';
-import ResultScene from './scenes/ResultScene.js?v=20260921-r49';
+import BootScene from './scenes/BootScene.js?v=20260921-r50';
+import CharacterSelectScene from './scenes/CharacterSelectScene.js?v=20260921-r50';
+import GarageScene from './scenes/GarageScene.js?v=20260921-r50';
+import MeetScene from './scenes/MeetScene.js?v=20260921-r50';
+import RaceScene from './scenes/RaceScene.js?v=20260921-r50';
+import ResultScene from './scenes/ResultScene.js?v=20260921-r50';
 
 
 // Phone readability pass.
