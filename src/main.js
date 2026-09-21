a7f36c62738434fedd602eabe6b54cfc24abf955
azyuen <azyuen@gmail.com>
2026-09-21 23:57:58 +1000

    Bump game scene modules to R79

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
-import BootScene from './scenes/BootScene.js?v=20260921-r78';
-import CharacterSelectScene from './scenes/CharacterSelectScene.js?v=20260921-r78';
-import GarageScene from './scenes/GarageScene.js?v=20260921-r78';
-import MeetScene from './scenes/MeetScene.js?v=20260921-r78';
-import RaceScene from './scenes/RaceScene.js?v=20260921-r78';
-import ResultScene from './scenes/ResultScene.js?v=20260921-r78';
+import BootScene from './scenes/BootScene.js?v=20260921-r79';
+import CharacterSelectScene from './scenes/CharacterSelectScene.js?v=20260921-r79';
+import GarageScene from './scenes/GarageScene.js?v=20260921-r79';
+import MeetScene from './scenes/MeetScene.js?v=20260921-r79';
+import RaceScene from './scenes/RaceScene.js?v=20260921-r79';
+import ResultScene from './scenes/ResultScene.js?v=20260921-r79';
 
 
 // Phone readability pass.
