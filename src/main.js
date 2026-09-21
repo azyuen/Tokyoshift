516286ad9a98a22a6e59fdf703c70c2ed1fbb754
azyuen <azyuen@gmail.com>
2026-09-21 14:04:26 +1000

    Bump final mechanics modules to R43

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
-import BootScene from './scenes/BootScene.js?v=20260921-r42';
-import CharacterSelectScene from './scenes/CharacterSelectScene.js?v=20260921-r42';
-import GarageScene from './scenes/GarageScene.js?v=20260921-r42';
-import MeetScene from './scenes/MeetScene.js?v=20260921-r42';
-import RaceScene from './scenes/RaceScene.js?v=20260921-r42';
-import ResultScene from './scenes/ResultScene.js?v=20260921-r42';
+import BootScene from './scenes/BootScene.js?v=20260921-r43';
+import CharacterSelectScene from './scenes/CharacterSelectScene.js?v=20260921-r43';
+import GarageScene from './scenes/GarageScene.js?v=20260921-r43';
+import MeetScene from './scenes/MeetScene.js?v=20260921-r43';
+import RaceScene from './scenes/RaceScene.js?v=20260921-r43';
+import ResultScene from './scenes/ResultScene.js?v=20260921-r43';
 
 
 // Phone readability pass.
