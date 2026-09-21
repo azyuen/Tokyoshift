1ea4bcbad4a47117412d24fa93f410012af76545
azyuen <azyuen@gmail.com>
2026-09-22 00:12:39 +1000

    R81: load revised vehicle audio

--- src/scenes/RaceScene.js
+++ src/scenes/RaceScene.js
@@ -18,7 +18,7 @@ import { characters } from '../data/characters.js?v=20260921-r43';
 import { WORKSHOP_RETURN_COST } from '../data/meetAssets.js?v=20260921-r76';
 import { saveSessionState, saveManualState, restoreManualSave, readManualSave, clearAllSaves } from '../state/GameState.js?v=20260921-r76';
 import { playRaceMusic, playVictorySting, stopMusic } from '../audio/MusicManager.js?v=20260921-r57';
-import EngineAudioSystem from '../audio/EngineAudioSystem.js?v=20260921-r70';
+import EngineAudioSystem from '../audio/EngineAudioSystem.js?v=20260921-r81';
 import {
   getEncounterAi,
   boostAiForPinkSlip,
