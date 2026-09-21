04fc944404c468c78956e27c0510af435bc33ef9
azyuen <azyuen@gmail.com>
2026-09-22 00:44:41 +1000

    R84: refresh scenes for new meet regions

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
-import BootScene from './scenes/BootScene.js?v=20260922-r83';
-import CharacterSelectScene from './scenes/CharacterSelectScene.js?v=20260922-r83';
-import GarageScene from './scenes/GarageScene.js?v=20260922-r83';
-import MeetScene from './scenes/MeetScene.js?v=20260922-r83';
-import RaceScene from './scenes/RaceScene.js?v=20260922-r83';
-import ResultScene from './scenes/ResultScene.js?v=20260922-r83';
+import BootScene from './scenes/BootScene.js?v=20260922-r84';
+import CharacterSelectScene from './scenes/CharacterSelectScene.js?v=20260922-r84';
+import GarageScene from './scenes/GarageScene.js?v=20260922-r84';
+import MeetScene from './scenes/MeetScene.js?v=20260922-r84';
+import RaceScene from './scenes/RaceScene.js?v=20260922-r84';
+import ResultScene from './scenes/ResultScene.js?v=20260922-r84';
 
 
 // Phone readability pass.
