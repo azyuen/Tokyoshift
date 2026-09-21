e0de1d99354dd5f7a40c7e5de9131beea41e4c04
azyuen <azyuen@gmail.com>
2026-09-21 16:57:31 +1000

    Unify game state module on R56

--- src/scenes/BootScene.js
+++ src/scenes/BootScene.js
@@ -1,6 +1,6 @@
 import { garageAssets } from '../data/garageAssets.js?v=20260921-r43';
 import { characters, characterOrder } from '../data/characters.js?v=20260921-r43';
-import { createDefaultGameState, readManualSave, applyStateToRegistry } from '../state/GameState.js?v=20260921-r55';
+import { createDefaultGameState, readManualSave, applyStateToRegistry } from '../state/GameState.js?v=20260921-r56';
 
 export default class BootScene extends Phaser.Scene {
   constructor() { super('BootScene'); }
