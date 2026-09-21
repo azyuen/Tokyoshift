9f22783282a4fab842774968d5f2a402dfb64f79
azyuen <azyuen@gmail.com>
2026-09-21 21:21:44 +1000

    R68 add stock engine sprite asset slots

--- src/data/garageAssets.js
+++ src/data/garageAssets.js
@@ -3,4 +3,10 @@ export const garageAssets = [
   { key: 'garageWorkshopTunerBg', path: 'assets/Garage/Workshop_tuner.PNG?v=20260921-r15' },
   { key: 'garageCharacterLeft', path: 'assets/Garage/character_left.svg' },
   { key: 'garageCharacterRight', path: 'assets/Garage/character_right.svg' },
+  { key: 'stockEngine4AGE', path: 'assets/Engines/4age.png?v=20260921-r68' },
+  { key: 'stockEngineRB26DETT', path: 'assets/Engines/rb26dett.png?v=20260921-r68' },
+  { key: 'stockEngine4G63T', path: 'assets/Engines/4g63t.png?v=20260921-r68' },
+  { key: 'stockEngine13BT', path: 'assets/Engines/13bt.png?v=20260921-r68' },
+  { key: 'stockEngineEJ22G', path: 'assets/Engines/ej22g.png?v=20260921-r68' },
+  { key: 'stockEngineB16B', path: 'assets/Engines/b16b.png?v=20260921-r68' },
 ];
