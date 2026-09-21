00fb84414337b594690c0d777ecf8304c79d5631
azyuen <azyuen@gmail.com>
2026-09-21 17:00:17 +1000

    Bump tuning and settings modules to R57

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
-import BootScene from './scenes/BootScene.js?v=20260921-r56';
-import CharacterSelectScene from './scenes/CharacterSelectScene.js?v=20260921-r56';
-import GarageScene from './scenes/GarageScene.js?v=20260921-r56';
-import MeetScene from './scenes/MeetScene.js?v=20260921-r56';
-import RaceScene from './scenes/RaceScene.js?v=20260921-r56';
-import ResultScene from './scenes/ResultScene.js?v=20260921-r56';
+import BootScene from './scenes/BootScene.js?v=20260921-r57';
+import CharacterSelectScene from './scenes/CharacterSelectScene.js?v=20260921-r57';
+import GarageScene from './scenes/GarageScene.js?v=20260921-r57';
+import MeetScene from './scenes/MeetScene.js?v=20260921-r57';
+import RaceScene from './scenes/RaceScene.js?v=20260921-r57';
+import ResultScene from './scenes/ResultScene.js?v=20260921-r57';
 
 
 // Phone readability pass.
