2b0bff38fe738b1a31305aebedba96c16e7db6b7
azyuen <azyuen@gmail.com>
2026-09-21 17:15:22 +1000

    Load polished Workshop and Meet scenes in R58

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
-import BootScene from './scenes/BootScene.js?v=20260921-r57';
-import CharacterSelectScene from './scenes/CharacterSelectScene.js?v=20260921-r57';
-import GarageScene from './scenes/GarageScene.js?v=20260921-r57';
-import MeetScene from './scenes/MeetScene.js?v=20260921-r57';
-import RaceScene from './scenes/RaceScene.js?v=20260921-r57';
-import ResultScene from './scenes/ResultScene.js?v=20260921-r57';
+import BootScene from './scenes/BootScene.js?v=20260921-r58';
+import CharacterSelectScene from './scenes/CharacterSelectScene.js?v=20260921-r58';
+import GarageScene from './scenes/GarageScene.js?v=20260921-r58';
+import MeetScene from './scenes/MeetScene.js?v=20260921-r58';
+import RaceScene from './scenes/RaceScene.js?v=20260921-r58';
+import ResultScene from './scenes/ResultScene.js?v=20260921-r58';
 
 
 // Phone readability pass.
