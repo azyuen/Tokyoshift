68231f36602b72ba55093a7fd92cf2986dabf22f
azyuen <azyuen@gmail.com>
2026-09-23 00:21:39 +1000

    Refresh garage modules for Ginza collector cars

--- src/scenes/GarageScene.js
+++ src/scenes/GarageScene.js
@@ -1,5 +1,5 @@
-import { cars, carOrder } from '../data/cars.js?v=20260922-r83';
-import { engines } from '../data/engines.js?v=20260921-r43';
+import { cars, carOrder } from '../data/cars.js?v=20260923-r132';
+import { engines } from '../data/engines.js?v=20260923-r132';
 import { characters } from '../data/characters.js?v=20260922-r111';
 import {
   ENGINE_PART_ORDER,
@@ -67,7 +67,7 @@ import {
   getCarBodyTextureKey,
   createCarBodyLayers,
   setCarBodyPaint,
-} from '../vehicles/CarAppearance.js?v=20260922-r83';
+} from '../vehicles/CarAppearance.js?v=20260923-r132';
 
 const PIXEL_FONT = '"Silkscreen", monospace';
 const BODY_FONT = '"Rajdhani", monospace';
