d2b54aea8318a54b82bdbad9f9c33b42fb8fb520
azyuen <azyuen@gmail.com>
2026-09-22 11:22:00 +1000

    R94 load tuning cosmetic update

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
-import BootScene from './scenes/BootScene.js?v=20260922-r93';
-import CharacterSelectScene from './scenes/CharacterSelectScene.js?v=20260922-r93';
-import GarageScene from './scenes/GarageScene.js?v=20260922-r93';
-import MeetScene from './scenes/MeetScene.js?v=20260922-r93';
-import RaceScene from './scenes/RaceScene.js?v=20260922-r93';
-import ResultScene from './scenes/ResultScene.js?v=20260922-r93';
+import BootScene from './scenes/BootScene.js?v=20260922-r94';
+import CharacterSelectScene from './scenes/CharacterSelectScene.js?v=20260922-r94';
+import GarageScene from './scenes/GarageScene.js?v=20260922-r94';
+import MeetScene from './scenes/MeetScene.js?v=20260922-r94';
+import RaceScene from './scenes/RaceScene.js?v=20260922-r94';
+import ResultScene from './scenes/ResultScene.js?v=20260922-r94';
 
 
 // Phone readability pass.
