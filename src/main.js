2c05305d0fa2a4810871056ba34eb3e0a5ad4c98
azyuen <azyuen@gmail.com>
2026-09-23 09:01:55 +1000

    R139: refresh progressive travel scenes

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
-import BootScene from './scenes/BootScene.js?v=20260923-r138';
+import BootScene from './scenes/BootScene.js?v=20260923-r139';
 import CharacterSelectScene from './scenes/CharacterSelectScene.js?v=20260923-r134';
-import GarageScene from './scenes/GarageScene.js?v=20260923-r138';
-import CentralTokyoScene from './scenes/CentralTokyoScene.js?v=20260923-r138';
-import MeetScene from './scenes/MeetScene.js?v=20260923-r134';
+import GarageScene from './scenes/GarageScene.js?v=20260923-r139';
+import CentralTokyoScene from './scenes/CentralTokyoScene.js?v=20260923-r139';
+import MeetScene from './scenes/MeetScene.js?v=20260923-r139';
 import RaceScene from './scenes/RaceScene.js?v=20260923-r138';
 import ResultScene from './scenes/ResultScene.js?v=20260922-r112';
 
