f39955c5d2a81b09798ab8de2dd5cbfa580bb1a3
azyuen <azyuen@gmail.com>
2026-09-22 17:36:43 +1000

    Bump character role data in boot

--- src/scenes/BootScene.js
+++ src/scenes/BootScene.js
@@ -1,8 +1,8 @@
 import { garageAssets } from '../data/garageAssets.js?v=20260922-r109';
 import { cars } from '../data/cars.js?v=20260922-r83';
 import { preloadCarAppearanceAssets } from '../vehicles/CarAppearance.js?v=20260922-r83';
-import { characters, characterOrder } from '../data/characters.js?v=20260921-r43';
-import { createDefaultGameState, readManualSave, readSessionState, applyStateToRegistry } from '../state/GameState.js?v=20260922-r108';
+import { characters, characterOrder } from '../data/characters.js?v=20260922-r111';
+import { createDefaultGameState, readManualSave, readSessionState, applyStateToRegistry } from '../state/GameState.js?v=20260922-r111';
 
 export default class BootScene extends Phaser.Scene {
   constructor() { super('BootScene'); }
