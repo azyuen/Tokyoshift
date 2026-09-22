05af4e66d18ff7e83864bd42c0d0451eeb30c575
azyuen <azyuen@gmail.com>
2026-09-23 09:01:51 +1000

    R139: load progressive travel map

--- src/scenes/CentralTokyoScene.js
+++ src/scenes/CentralTokyoScene.js
@@ -17,8 +17,8 @@ import { createDriverSilhouette } from '../vehicles/DriverSilhouette.js?v=202609
 import { createVisualModLayers } from '../data/visualMods.js?v=20260923-r138';
 import { getEncounterAi } from '../data/encounterProfiles.js?v=20260921-r76';
 import { saveSessionState } from '../state/GameState.js?v=20260922-r131';
-import { showTravelMap } from '../ui/TravelMap.js?v=20260922-r131';
-import { getTravelLocation } from '../data/travelRegions.js?v=20260922-r131';
+import { showTravelMap } from '../ui/TravelMap.js?v=20260923-r139';
+import { getTravelLocation } from '../data/travelRegions.js?v=20260923-r139';
 import {
   getGarageCapacity,
   getUnlockedWorkshops,
