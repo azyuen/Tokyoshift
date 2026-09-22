3c556a8a2f06127d4f09f36524a046f9bc4d3c97
azyuen <azyuen@gmail.com>
2026-09-22 20:22:33 +1000

    R122 bump caches

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-const BUILD = 'R121';
-const ASSET_CACHE = 'tokyoshift-assets-v67';
-const RUNTIME_CACHE = 'tokyoshift-runtime-v67';
+const BUILD = 'R122';
+const ASSET_CACHE = 'tokyoshift-assets-v68';
+const RUNTIME_CACHE = 'tokyoshift-runtime-v68';
 
 self.addEventListener('install', () => self.skipWaiting());
 
