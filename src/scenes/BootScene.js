1358c93002368d3412f76a144e3061d43a577020
azyuen <azyuen@gmail.com>
2026-09-22 16:10:48 +1000

    R106 preload corrected turbo assets

--- src/scenes/BootScene.js
+++ src/scenes/BootScene.js
@@ -1,4 +1,4 @@
-import { garageAssets } from '../data/garageAssets.js?v=20260922-r100';
+import { garageAssets } from '../data/garageAssets.js?v=20260922-r106';
 import { cars } from '../data/cars.js?v=20260922-r83';
 import { preloadCarAppearanceAssets } from '../vehicles/CarAppearance.js?v=20260922-r83';
 import { characters, characterOrder } from '../data/characters.js?v=20260921-r43';
