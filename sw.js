76d335ee13045bb45b7bacd2649a83c3d29a4eff
azyuen <azyuen@gmail.com>
2026-09-22 10:32:14 +1000

    R87: refresh PWA caches

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-const BUILD = 'R86';
-const ASSET_CACHE = 'tokyoshift-assets-v44';
-const RUNTIME_CACHE = 'tokyoshift-runtime-v44';
+const BUILD = 'R87';
+const ASSET_CACHE = 'tokyoshift-assets-v45';
+const RUNTIME_CACHE = 'tokyoshift-runtime-v45';
 
 self.addEventListener('install', () => self.skipWaiting());
 
