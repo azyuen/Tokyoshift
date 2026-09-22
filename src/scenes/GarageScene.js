8f394d19cfddfac181eb81b82aa0aa4d0fba1254
azyuen <azyuen@gmail.com>
2026-09-22 22:31:53 +1000

    R126: refresh dev-profile access state

--- src/scenes/GarageScene.js
+++ src/scenes/GarageScene.js
@@ -26,7 +26,7 @@ import {
   getExhaustNosCartCost,
   applySecondaryTuning,
 } from '../data/secondaryTuning.js?v=20260922-r124';
-import { saveManualState, saveSessionState } from '../state/GameState.js?v=20260922-r125';
+import { saveManualState, saveSessionState } from '../state/GameState.js?v=20260922-r126';
 import { addSettingsButton, showSettingsPanel } from '../ui/SettingsPanel.js?v=20260922-r125';
 import { getMeetLocation } from '../data/meetAssets.js?v=20260922-r84';
 import { getTravelLocation } from '../data/travelRegions.js?v=20260922-r125';
@@ -34,7 +34,7 @@ import { showTravelMap } from '../ui/TravelMap.js?v=20260922-r125';
 import {
   CENTRAL_TOKYO_LOCATIONS,
   getPendingCentralTokyoInvite,
-} from '../data/centralTokyo.js?v=20260922-r125';
+} from '../data/centralTokyo.js?v=20260922-r126';
 import { playMusic } from '../audio/MusicManager.js?v=20260922-r99';
 import {
   getGarageCapacity,
