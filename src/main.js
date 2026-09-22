c54282f81c84f6aca893bb231957579cfb73e2b7
azyuen <azyuen@gmail.com>
2026-09-23 09:45:35 +1000

    Publish R141 Odaiba team and race pose system

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
-import BootScene from './scenes/BootScene.js?v=20260923-r140';
-import CharacterSelectScene from './scenes/CharacterSelectScene.js?v=20260923-r140';
-import GarageScene from './scenes/GarageScene.js?v=20260923-r140';
-import CentralTokyoScene from './scenes/CentralTokyoScene.js?v=20260923-r140';
-import MeetScene from './scenes/MeetScene.js?v=20260923-r140';
-import RaceScene from './scenes/RaceScene.js?v=20260923-r140';
+import BootScene from './scenes/BootScene.js?v=20260923-r141';
+import CharacterSelectScene from './scenes/CharacterSelectScene.js?v=20260923-r141';
+import GarageScene from './scenes/GarageScene.js?v=20260923-r141';
+import CentralTokyoScene from './scenes/CentralTokyoScene.js?v=20260923-r141';
+import MeetScene from './scenes/MeetScene.js?v=20260923-r141';
+import RaceScene from './scenes/RaceScene.js?v=20260923-r141';
 import ResultScene from './scenes/ResultScene.js?v=20260922-r112';
 
 
