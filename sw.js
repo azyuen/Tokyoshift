867b5ca071099cea776d01df1c617ac2a98bf111
azyuen <azyuen@gmail.com>
2026-09-22 21:20:31 +1000

    R125: refresh Central Tokyo caches

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-const BUILD = 'R124';
-const ASSET_CACHE = 'tokyoshift-assets-v70';
-const RUNTIME_CACHE = 'tokyoshift-runtime-v70';
+const BUILD = 'R125';
+const ASSET_CACHE = 'tokyoshift-assets-v71';
+const RUNTIME_CACHE = 'tokyoshift-runtime-v71';
 
 self.addEventListener('install', () => self.skipWaiting());
 
