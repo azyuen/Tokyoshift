43979c629739ccf8d3188be9b43c0c121fe70d39
azyuen <azyuen@gmail.com>
2026-09-23 09:38:59 +1000

    Bump meet save-state schema

--- src/scenes/MeetScene.js
+++ src/scenes/MeetScene.js
@@ -21,7 +21,7 @@ import {
   WORKSHOP_RETURN_COST,
 } from '../data/meetAssets.js?v=20260922-r84';
 import { playMusic } from '../audio/MusicManager.js?v=20260922-r99';
-import { saveSessionState } from '../state/GameState.js?v=20260922-r131';
+import { saveSessionState } from '../state/GameState.js?v=20260923-r140';
 import { addSettingsButton } from '../ui/SettingsPanel.js?v=20260922-r125';
 import { showTravelMap } from '../ui/TravelMap.js?v=20260923-r139';
 import { getTravelLocation } from '../data/travelRegions.js?v=20260923-r139';
