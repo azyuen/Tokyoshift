d37e6e2a926076def1899eb429913670b49cb3da
azyuen <azyuen@gmail.com>
2026-09-22 10:56:23 +1000

    R90: refresh garage scene

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
-import BootScene from './scenes/BootScene.js?v=20260922-r89';
-import CharacterSelectScene from './scenes/CharacterSelectScene.js?v=20260922-r89';
-import GarageScene from './scenes/GarageScene.js?v=20260922-r89';
-import MeetScene from './scenes/MeetScene.js?v=20260922-r89';
-import RaceScene from './scenes/RaceScene.js?v=20260922-r89';
-import ResultScene from './scenes/ResultScene.js?v=20260922-r89';
+import BootScene from './scenes/BootScene.js?v=20260922-r90';
+import CharacterSelectScene from './scenes/CharacterSelectScene.js?v=20260922-r90';
+import GarageScene from './scenes/GarageScene.js?v=20260922-r90';
+import MeetScene from './scenes/MeetScene.js?v=20260922-r90';
+import RaceScene from './scenes/RaceScene.js?v=20260922-r90';
+import ResultScene from './scenes/ResultScene.js?v=20260922-r90';
 
 
 // Phone readability pass.
