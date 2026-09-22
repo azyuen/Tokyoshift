23be1e69e7a736ad6eb2c0416d9bd30ad9666564
azyuen <azyuen@gmail.com>
2026-09-22 12:28:16 +1000

    R97 load map-only meet navigation

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
-import BootScene from './scenes/BootScene.js?v=20260922-r96';
-import CharacterSelectScene from './scenes/CharacterSelectScene.js?v=20260922-r96';
-import GarageScene from './scenes/GarageScene.js?v=20260922-r96';
-import MeetScene from './scenes/MeetScene.js?v=20260922-r96';
-import RaceScene from './scenes/RaceScene.js?v=20260922-r96';
-import ResultScene from './scenes/ResultScene.js?v=20260922-r96';
+import BootScene from './scenes/BootScene.js?v=20260922-r97';
+import CharacterSelectScene from './scenes/CharacterSelectScene.js?v=20260922-r97';
+import GarageScene from './scenes/GarageScene.js?v=20260922-r97';
+import MeetScene from './scenes/MeetScene.js?v=20260922-r97';
+import RaceScene from './scenes/RaceScene.js?v=20260922-r97';
+import ResultScene from './scenes/ResultScene.js?v=20260922-r97';
 
 
 // Phone readability pass.
