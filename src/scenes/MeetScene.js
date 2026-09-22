3347b30420931190c4cfdab6a241c4eea7165ef7
azyuen <azyuen@gmail.com>
2026-09-22 19:16:11 +1000

    R117 refresh fixed settings panel

--- src/scenes/MeetScene.js
+++ src/scenes/MeetScene.js
@@ -18,7 +18,7 @@ import {
 } from '../data/meetAssets.js?v=20260922-r84';
 import { playMusic } from '../audio/MusicManager.js?v=20260922-r99';
 import { saveSessionState } from '../state/GameState.js?v=20260922-r115';
-import { addSettingsButton } from '../ui/SettingsPanel.js?v=20260922-r115';
+import { addSettingsButton } from '../ui/SettingsPanel.js?v=20260922-r117';
 import { showTravelMap } from '../ui/TravelMap.js?v=20260922-r98';
 import { getGarageCapacity, getUnlockedWorkshops, getCarsInWorkshop, isWorkshopUnlocked } from '../data/workshopProgression.js?v=20260922-r98';
 import { startSceneLoading, finishSceneLoading } from '../ui/LoadingScreen.js?v=20260922-r117';
