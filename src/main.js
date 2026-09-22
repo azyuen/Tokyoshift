5eecda9e08b11f08525dd20b07a58fbb0d7c6e23
azyuen <azyuen@gmail.com>
2026-09-22 12:19:34 +1000

    R96 load home-return fix

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
-import BootScene from './scenes/BootScene.js?v=20260922-r95';
-import CharacterSelectScene from './scenes/CharacterSelectScene.js?v=20260922-r95';
-import GarageScene from './scenes/GarageScene.js?v=20260922-r95';
-import MeetScene from './scenes/MeetScene.js?v=20260922-r95';
-import RaceScene from './scenes/RaceScene.js?v=20260922-r95';
-import ResultScene from './scenes/ResultScene.js?v=20260922-r95';
+import BootScene from './scenes/BootScene.js?v=20260922-r96';
+import CharacterSelectScene from './scenes/CharacterSelectScene.js?v=20260922-r96';
+import GarageScene from './scenes/GarageScene.js?v=20260922-r96';
+import MeetScene from './scenes/MeetScene.js?v=20260922-r96';
+import RaceScene from './scenes/RaceScene.js?v=20260922-r96';
+import ResultScene from './scenes/ResultScene.js?v=20260922-r96';
 
 
 // Phone readability pass.
