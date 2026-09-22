8845a8fc153fbe44cca9e0f1db9e3edad76a8ea6
azyuen <azyuen@gmail.com>
2026-09-23 01:24:31 +1000

    Bump gameplay scenes for custom hero wheels R134

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
-import BootScene from './scenes/BootScene.js?v=20260923-r133';
-import CharacterSelectScene from './scenes/CharacterSelectScene.js?v=20260923-r132';
-import GarageScene from './scenes/GarageScene.js?v=20260923-r133';
-import CentralTokyoScene from './scenes/CentralTokyoScene.js?v=20260923-r133';
-import MeetScene from './scenes/MeetScene.js?v=20260923-r133';
-import RaceScene from './scenes/RaceScene.js?v=20260923-r133';
+import BootScene from './scenes/BootScene.js?v=20260923-r134';
+import CharacterSelectScene from './scenes/CharacterSelectScene.js?v=20260923-r134';
+import GarageScene from './scenes/GarageScene.js?v=20260923-r134';
+import CentralTokyoScene from './scenes/CentralTokyoScene.js?v=20260923-r134';
+import MeetScene from './scenes/MeetScene.js?v=20260923-r134';
+import RaceScene from './scenes/RaceScene.js?v=20260923-r134';
 import ResultScene from './scenes/ResultScene.js?v=20260922-r112';
 
 
