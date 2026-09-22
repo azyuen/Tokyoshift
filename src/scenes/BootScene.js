d5b1d816b8f0cd8eeb2d7d7ef94a55bf287f37b2
azyuen <azyuen@gmail.com>
2026-09-23 06:43:35 +1000

    Refresh boot assets for Evo visual mods R138

--- src/scenes/BootScene.js
+++ src/scenes/BootScene.js
@@ -1,5 +1,5 @@
 import { garageAssets } from '../data/garageAssets.js?v=20260922-r128';
-import { cars } from '../data/cars.js?v=20260923-r134';
+import { cars } from '../data/cars.js?v=20260923-r137';
 import { preloadCarAppearanceAssets } from '../vehicles/CarAppearance.js?v=20260923-r134';
 import { characters, characterOrder } from '../data/characters.js?v=20260922-r111';
 import { createDefaultGameState, readManualSave, readSessionState, applyStateToRegistry } from '../state/GameState.js?v=20260922-r131';
@@ -36,7 +36,7 @@ export default class BootScene extends Phaser.Scene {
 
     // Standard cars load body + tintable paint + overlay. Ginza hero cars set
     // visual.singleBody and load only their finished one-off body PNG.
-    preloadCarAppearanceAssets(this, cars, '20260923-r134');
+    preloadCarAppearanceAssets(this, cars, '20260923-r138');
 
     this.load.image('wheel8Spoke', 'assets/wheels/wheel_8spoke.png');
     this.load.image('wheel5Spoke', 'assets/wheels/wheel_5spoke.png');
