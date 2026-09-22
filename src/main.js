349ebbaae2cb65e8deff86952ef6308a71dc2401
azyuen <azyuen@gmail.com>
2026-09-23 06:17:10 +1000

    Bump driver anchor system to R137

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
 import BootScene from './scenes/BootScene.js?v=20260923-r134';
 import CharacterSelectScene from './scenes/CharacterSelectScene.js?v=20260923-r134';
 import GarageScene from './scenes/GarageScene.js?v=20260923-r134';
-import CentralTokyoScene from './scenes/CentralTokyoScene.js?v=20260923-r136';
+import CentralTokyoScene from './scenes/CentralTokyoScene.js?v=20260923-r137';
 import MeetScene from './scenes/MeetScene.js?v=20260923-r134';
-import RaceScene from './scenes/RaceScene.js?v=20260923-r136';
+import RaceScene from './scenes/RaceScene.js?v=20260923-r137';
 import ResultScene from './scenes/ResultScene.js?v=20260922-r112';
 
 
