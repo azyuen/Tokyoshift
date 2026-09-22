579d63719dd7b400112980a7d8c87f618fe0f35d
azyuen <azyuen@gmail.com>
2026-09-22 21:15:42 +1000

    R125: preserve Central Tokyo state

--- src/scenes/BootScene.js
+++ src/scenes/BootScene.js
@@ -2,7 +2,7 @@ import { garageAssets } from '../data/garageAssets.js?v=20260922-r124';
 import { cars } from '../data/cars.js?v=20260922-r83';
 import { preloadCarAppearanceAssets } from '../vehicles/CarAppearance.js?v=20260922-r83';
 import { characters, characterOrder } from '../data/characters.js?v=20260922-r111';
-import { createDefaultGameState, readManualSave, readSessionState, applyStateToRegistry } from '../state/GameState.js?v=20260922-r122';
+import { createDefaultGameState, readManualSave, readSessionState, applyStateToRegistry } from '../state/GameState.js?v=20260922-r125';
 import { startSceneLoading } from '../ui/LoadingScreen.js?v=20260922-r122';
 
 export default class BootScene extends Phaser.Scene {
