0e4c42876eb548cd0ae8dec1d8a8d89682411309
azyuen <azyuen@gmail.com>
2026-09-22 16:45:46 +1000

    Load challenger and competition systems in R108

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
-import BootScene from './scenes/BootScene.js?v=20260922-r107';
-import CharacterSelectScene from './scenes/CharacterSelectScene.js?v=20260922-r106';
-import GarageScene from './scenes/GarageScene.js?v=20260922-r107';
-import MeetScene from './scenes/MeetScene.js?v=20260922-r106';
-import RaceScene from './scenes/RaceScene.js?v=20260922-r106';
-import ResultScene from './scenes/ResultScene.js?v=20260922-r106';
+import BootScene from './scenes/BootScene.js?v=20260922-r108';
+import CharacterSelectScene from './scenes/CharacterSelectScene.js?v=20260922-r108';
+import GarageScene from './scenes/GarageScene.js?v=20260922-r108';
+import MeetScene from './scenes/MeetScene.js?v=20260922-r108';
+import RaceScene from './scenes/RaceScene.js?v=20260922-r108';
+import ResultScene from './scenes/ResultScene.js?v=20260922-r108';
 
 
 // Phone readability pass.
