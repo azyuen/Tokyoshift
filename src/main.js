e6f90ba186791ce06e90819c7db4f6071bd3252b
azyuen <azyuen@gmail.com>
2026-09-22 18:58:44 +1000

    R115 load profile-aware scenes

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
-import BootScene from './scenes/BootScene.js?v=20260922-r112';
-import CharacterSelectScene from './scenes/CharacterSelectScene.js?v=20260922-r112';
-import GarageScene from './scenes/GarageScene.js?v=20260922-r114';
-import MeetScene from './scenes/MeetScene.js?v=20260922-r112';
-import RaceScene from './scenes/RaceScene.js?v=20260922-r112';
+import BootScene from './scenes/BootScene.js?v=20260922-r115';
+import CharacterSelectScene from './scenes/CharacterSelectScene.js?v=20260922-r115';
+import GarageScene from './scenes/GarageScene.js?v=20260922-r115';
+import MeetScene from './scenes/MeetScene.js?v=20260922-r115';
+import RaceScene from './scenes/RaceScene.js?v=20260922-r115';
 import ResultScene from './scenes/ResultScene.js?v=20260922-r112';
 
 
