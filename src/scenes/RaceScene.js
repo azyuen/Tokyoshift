54c140f24e2879e4ed781c21664adb07db03e9f5
azyuen <azyuen@gmail.com>
2026-09-22 19:00:12 +1000

    R116 refresh reusable loader helper

--- src/scenes/RaceScene.js
+++ src/scenes/RaceScene.js
@@ -19,7 +19,7 @@ import { WORKSHOP_RETURN_COST } from '../data/meetAssets.js?v=20260922-r84';
 import { saveSessionState, saveManualState, restoreManualSave, readManualSave, clearAllSaves } from '../state/GameState.js?v=20260922-r115';
 import { playRaceMusic, playVictorySting, stopMusic } from '../audio/MusicManager.js?v=20260922-r99';
 import EngineAudioSystem from '../audio/EngineAudioSystem.js?v=20260921-r81';
-import { startSceneLoading, finishSceneLoading } from '../ui/LoadingScreen.js?v=20260922-r115';
+import { startSceneLoading, finishSceneLoading } from '../ui/LoadingScreen.js?v=20260922-r116';
 import {
   getEncounterAi,
   boostAiForPinkSlip,
