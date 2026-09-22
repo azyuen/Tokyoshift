9a35b215c0b1d587e2f92744a1d2a8d16a6820ed
azyuen <azyuen@gmail.com>
2026-09-22 23:18:53 +1000

    R128: use current Tokyo unlock map from home workshop

--- src/scenes/GarageScene.js
+++ src/scenes/GarageScene.js
@@ -32,7 +32,7 @@ import {
   getExhaustNosCartCost,
   applySecondaryTuning,
 } from '../data/secondaryTuning.js?v=20260922-r128';
-import { saveManualState, saveSessionState } from '../state/GameState.js?v=20260922-r127';
+import { saveManualState, saveSessionState } from '../state/GameState.js?v=20260922-r128';
 import { addSettingsButton, showSettingsPanel } from '../ui/SettingsPanel.js?v=20260922-r125';
 import { getMeetLocation } from '../data/meetAssets.js?v=20260922-r84';
 import { getTravelLocation } from '../data/travelRegions.js?v=20260922-r125';
@@ -40,7 +40,7 @@ import { showTravelMap } from '../ui/TravelMap.js?v=20260922-r127';
 import {
   CENTRAL_TOKYO_LOCATIONS,
   getPendingCentralTokyoInvite,
-} from '../data/centralTokyo.js?v=20260922-r127';
+} from '../data/centralTokyo.js?v=20260922-r128';
 import { playMusic } from '../audio/MusicManager.js?v=20260922-r99';
 import {
   getGarageCapacity,
