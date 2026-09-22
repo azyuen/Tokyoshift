89ada9caadc65969be0cced8ef65d3b25bfaddc5
azyuen <azyuen@gmail.com>
2026-09-22 19:16:08 +1000

    R117 refresh fixed settings panel

--- src/scenes/GarageScene.js
+++ src/scenes/GarageScene.js
@@ -27,7 +27,7 @@ import {
   applySecondaryTuning,
 } from '../data/secondaryTuning.js?v=20260922-r114';
 import { saveManualState, saveSessionState } from '../state/GameState.js?v=20260922-r115';
-import { addSettingsButton } from '../ui/SettingsPanel.js?v=20260922-r115';
+import { addSettingsButton } from '../ui/SettingsPanel.js?v=20260922-r117';
 import { getMeetLocation } from '../data/meetAssets.js?v=20260922-r84';
 import { showTravelMap } from '../ui/TravelMap.js?v=20260922-r97';
 import { playMusic } from '../audio/MusicManager.js?v=20260922-r99';
