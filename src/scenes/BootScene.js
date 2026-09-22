2628d1f6a34e46d98464ed23076f7b388769f8f7
azyuen <azyuen@gmail.com>
2026-09-22 21:16:56 +1000

    R125: refresh loading/state modules

--- src/scenes/BootScene.js
+++ src/scenes/BootScene.js
@@ -3,7 +3,7 @@ import { cars } from '../data/cars.js?v=20260922-r83';
 import { preloadCarAppearanceAssets } from '../vehicles/CarAppearance.js?v=20260922-r83';
 import { characters, characterOrder } from '../data/characters.js?v=20260922-r111';
 import { createDefaultGameState, readManualSave, readSessionState, applyStateToRegistry } from '../state/GameState.js?v=20260922-r125';
-import { startSceneLoading } from '../ui/LoadingScreen.js?v=20260922-r122';
+import { startSceneLoading } from '../ui/LoadingScreen.js?v=20260922-r125';
 
 export default class BootScene extends Phaser.Scene {
   constructor() { super('BootScene'); }
