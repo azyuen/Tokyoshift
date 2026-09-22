db2721f7098a5d0e380a123af4872af3d0a128a1
azyuen <azyuen@gmail.com>
2026-09-22 19:35:31 +1000

    R118 load regression fixes

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import BootScene from './scenes/BootScene.js?v=20260922-r117';
-import CharacterSelectScene from './scenes/CharacterSelectScene.js?v=20260922-r117';
-import GarageScene from './scenes/GarageScene.js?v=20260922-r117';
-import MeetScene from './scenes/MeetScene.js?v=20260922-r117';
+import CharacterSelectScene from './scenes/CharacterSelectScene.js?v=20260922-r118';
+import GarageScene from './scenes/GarageScene.js?v=20260922-r118';
+import MeetScene from './scenes/MeetScene.js?v=20260922-r118';
 import RaceScene from './scenes/RaceScene.js?v=20260922-r117';
 import ResultScene from './scenes/ResultScene.js?v=20260922-r112';
 
