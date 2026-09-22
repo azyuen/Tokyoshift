48a3b0316192631c72820d7b014215b09e7d5094
azyuen <azyuen@gmail.com>
2026-09-23 01:24:18 +1000

    Bump hero wheel mappings to R134 in RaceScene.js

--- src/scenes/RaceScene.js
+++ src/scenes/RaceScene.js
@@ -4,14 +4,14 @@ import DragRacingAI from '../ai/DragRacingAI.js?v=20260921-r43';
 import RaceHUD from '../ui/RaceHUD.js?v=20260921-r43';
 import DebugHUD from '../ui/DebugHUD.js';
 import TokyoExpresswayBackground from '../environment/TokyoExpresswayBackground.js?v=20260921-r49';
-import { cars, carOrder } from '../data/cars.js?v=20260923-r133';
+import { cars, carOrder } from '../data/cars.js?v=20260923-r134';
 import {
   DEFAULT_PAINT_COLOR,
   getCarPaintColor,
   normalisePaintColor,
   createCarBodyLayers,
-} from '../vehicles/CarAppearance.js?v=20260923-r133';
-import { engines } from '../data/engines.js?v=20260923-r133';
+} from '../vehicles/CarAppearance.js?v=20260923-r134';
+import { engines } from '../data/engines.js?v=20260923-r134';
 import { applyEngineTuning } from '../data/tuning.js?v=20260921-r55';
 import { applySecondaryTuning, getExhaustNosTuning } from '../data/secondaryTuning.js?v=20260922-r128';
 import { characters, playableCharacterOrder, rivalCharacterOrder } from '../data/characters.js?v=20260922-r111';
