8e9d2da24134f280b17718630a0327fd2d19b54e
azyuen <azyuen@gmail.com>
2026-09-22 20:35:06 +1000

    R124 use cleaned drivetrain tuning in races

--- src/scenes/RaceScene.js
+++ src/scenes/RaceScene.js
@@ -13,7 +13,7 @@ import {
 } from '../vehicles/CarAppearance.js?v=20260922-r83';
 import { engines } from '../data/engines.js?v=20260921-r43';
 import { applyEngineTuning } from '../data/tuning.js?v=20260921-r55';
-import { applySecondaryTuning, getExhaustNosTuning } from '../data/secondaryTuning.js?v=20260921-r66';
+import { applySecondaryTuning, getExhaustNosTuning } from '../data/secondaryTuning.js?v=20260922-r124';
 import { characters, playableCharacterOrder, rivalCharacterOrder } from '../data/characters.js?v=20260922-r111';
 import { WORKSHOP_RETURN_COST } from '../data/meetAssets.js?v=20260922-r84';
 import { saveSessionState, saveManualState, restoreManualSave, readManualSave, clearAllSaves } from '../state/GameState.js?v=20260922-r115';
