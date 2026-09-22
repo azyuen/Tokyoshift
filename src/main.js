6a0b4663c5604f6928a8da31ee04b01e8600513f
azyuen <azyuen@gmail.com>
2026-09-22 17:37:01 +1000

    Bump driver role scenes to R111

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
-import BootScene from './scenes/BootScene.js?v=20260922-r110';
-import CharacterSelectScene from './scenes/CharacterSelectScene.js?v=20260922-r108';
-import GarageScene from './scenes/GarageScene.js?v=20260922-r110';
-import MeetScene from './scenes/MeetScene.js?v=20260922-r108';
-import RaceScene from './scenes/RaceScene.js?v=20260922-r108';
+import BootScene from './scenes/BootScene.js?v=20260922-r111';
+import CharacterSelectScene from './scenes/CharacterSelectScene.js?v=20260922-r111';
+import GarageScene from './scenes/GarageScene.js?v=20260922-r111';
+import MeetScene from './scenes/MeetScene.js?v=20260922-r111';
+import RaceScene from './scenes/RaceScene.js?v=20260922-r111';
 import ResultScene from './scenes/ResultScene.js?v=20260922-r108';
 
 
