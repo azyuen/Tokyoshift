0d8a9402d9b238181ee2ca0cc26c2f399c9b9e2f
azyuen <azyuen@gmail.com>
2026-09-22 16:45:28 +1000

    Refresh progression state for R108

--- src/scenes/BootScene.js
+++ src/scenes/BootScene.js
@@ -2,7 +2,7 @@ import { garageAssets } from '../data/garageAssets.js?v=20260922-r107';
 import { cars } from '../data/cars.js?v=20260922-r83';
 import { preloadCarAppearanceAssets } from '../vehicles/CarAppearance.js?v=20260922-r83';
 import { characters, characterOrder } from '../data/characters.js?v=20260921-r43';
-import { createDefaultGameState, readManualSave, readSessionState, applyStateToRegistry } from '../state/GameState.js?v=20260922-r106';
+import { createDefaultGameState, readManualSave, readSessionState, applyStateToRegistry } from '../state/GameState.js?v=20260922-r108';
 
 export default class BootScene extends Phaser.Scene {
   constructor() { super('BootScene'); }
