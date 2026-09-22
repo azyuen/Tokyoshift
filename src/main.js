e9a4c768d050f421f8929830c3bbec6e1bd21273
azyuen <azyuen@gmail.com>
2026-09-22 20:35:28 +1000

    R124 load drivetrain sprite and launch cleanup

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
-import BootScene from './scenes/BootScene.js?v=20260922-r122';
+import BootScene from './scenes/BootScene.js?v=20260922-r124';
 import CharacterSelectScene from './scenes/CharacterSelectScene.js?v=20260922-r120';
-import GarageScene from './scenes/GarageScene.js?v=20260922-r123';
+import GarageScene from './scenes/GarageScene.js?v=20260922-r124';
 import MeetScene from './scenes/MeetScene.js?v=20260922-r123';
-import RaceScene from './scenes/RaceScene.js?v=20260922-r117';
+import RaceScene from './scenes/RaceScene.js?v=20260922-r124';
 import ResultScene from './scenes/ResultScene.js?v=20260922-r112';
 
 
