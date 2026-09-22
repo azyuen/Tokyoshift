d9d81178d5e702ec760430e8ec1d62f245e3b271
azyuen <azyuen@gmail.com>
2026-09-23 00:21:50 +1000

    Enable collector car physics and rendering in races

--- src/scenes/RaceScene.js
+++ src/scenes/RaceScene.js
@@ -4,14 +4,14 @@ import DragRacingAI from '../ai/DragRacingAI.js?v=20260921-r43';
 import RaceHUD from '../ui/RaceHUD.js?v=20260921-r43';
 import DebugHUD from '../ui/DebugHUD.js';
 import TokyoExpresswayBackground from '../environment/TokyoExpresswayBackground.js?v=20260921-r49';
-import { cars, carOrder } from '../data/cars.js?v=20260922-r83';
+import { cars, carOrder } from '../data/cars.js?v=20260923-r132';
 import {
   DEFAULT_PAINT_COLOR,
   getCarPaintColor,
   normalisePaintColor,
   createCarBodyLayers,
-} from '../vehicles/CarAppearance.js?v=20260922-r83';
-import { engines } from '../data/engines.js?v=20260921-r43';
+} from '../vehicles/CarAppearance.js?v=20260923-r132';
+import { engines } from '../data/engines.js?v=20260923-r132';
 import { applyEngineTuning } from '../data/tuning.js?v=20260921-r55';
 import { applySecondaryTuning, getExhaustNosTuning } from '../data/secondaryTuning.js?v=20260922-r128';
 import { characters, playableCharacterOrder, rivalCharacterOrder } from '../data/characters.js?v=20260922-r111';
