9aac6ada794666d3dc37b40b4b1ec468c2e6d9c0
azyuen <azyuen@gmail.com>
2026-09-22 11:03:22 +1000

    R91: load safe workshop map actions

--- src/scenes/GarageScene.js
+++ src/scenes/GarageScene.js
@@ -29,7 +29,7 @@ import {
 import { saveManualState, saveSessionState } from '../state/GameState.js?v=20260922-r86';
 import { addSettingsButton } from '../ui/SettingsPanel.js?v=20260922-r86';
 import { getMeetLocation } from '../data/meetAssets.js?v=20260922-r84';
-import { showTravelMap } from '../ui/TravelMap.js?v=20260922-r86';
+import { showTravelMap } from '../ui/TravelMap.js?v=20260922-r91';
 import { playMusic } from '../audio/MusicManager.js?v=20260921-r57';
 import {
   getGarageCapacity,
