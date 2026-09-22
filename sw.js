ac9045c2b7172738e7ab3a808372539e7c7786f6
azyuen <azyuen@gmail.com>
2026-09-22 15:42:39 +1000

    Refresh R105 service-worker caches

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-const BUILD = 'R104';
-const ASSET_CACHE = 'tokyoshift-assets-v61';
-const RUNTIME_CACHE = 'tokyoshift-runtime-v61';
+const BUILD = 'R105';
+const ASSET_CACHE = 'tokyoshift-assets-v62';
+const RUNTIME_CACHE = 'tokyoshift-runtime-v62';
 
 self.addEventListener('install', () => self.skipWaiting());
 
