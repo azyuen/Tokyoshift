f1832078b581f63bceeba374d01e05b5b4e0cc43
azyuen <azyuen@gmail.com>
2026-09-22 11:03:33 +1000

    R91: refresh travel map scene imports

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
-import BootScene from './scenes/BootScene.js?v=20260922-r90';
-import CharacterSelectScene from './scenes/CharacterSelectScene.js?v=20260922-r90';
-import GarageScene from './scenes/GarageScene.js?v=20260922-r90';
-import MeetScene from './scenes/MeetScene.js?v=20260922-r90';
-import RaceScene from './scenes/RaceScene.js?v=20260922-r90';
-import ResultScene from './scenes/ResultScene.js?v=20260922-r90';
+import BootScene from './scenes/BootScene.js?v=20260922-r91';
+import CharacterSelectScene from './scenes/CharacterSelectScene.js?v=20260922-r91';
+import GarageScene from './scenes/GarageScene.js?v=20260922-r91';
+import MeetScene from './scenes/MeetScene.js?v=20260922-r91';
+import RaceScene from './scenes/RaceScene.js?v=20260922-r91';
+import ResultScene from './scenes/ResultScene.js?v=20260922-r91';
 
 
 // Phone readability pass.
