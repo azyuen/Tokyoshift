3fc9d97a278a96ba96a7c56f2bdcd4a7171a1757
azyuen <azyuen@gmail.com>
2026-09-24 08:13:23 +1000

    R164 load new hero car data in EngineTuningScene.js

--- src/scenes/EngineTuningScene.js
+++ src/scenes/EngineTuningScene.js
@@ -1,10 +1,10 @@
-import { cars } from '../data/cars.js?v=20260923-r158';
+import { cars } from '../data/cars.js?v=20260924-r164';
 import {
   getCarBodyTextureKey,
   createCarBodyLayers,
   getCarPaintColor,
 } from '../vehicles/CarAppearance.js?v=20260923-r154';
-import { engines } from '../data/engines.js?v=20260923-r134';
+import { engines } from '../data/engines.js?v=20260924-r164';
 import { getWheelPairFit } from '../vehicles/WheelFit.js?v=20260923-r152';
 import { characters } from '../data/characters.js?v=20260921-r43';
 import {
