573be733ebe5705a8caa8da4aa907a9c9fb41d9d
azyuen <azyuen@gmail.com>
2026-09-24 08:41:12 +1000

    R165 load recalibrated 787B in MeetScene.js

--- src/scenes/MeetScene.js
+++ src/scenes/MeetScene.js
@@ -1,4 +1,4 @@
-import { cars, carOrder } from '../data/cars.js?v=20260924-r164';
+import { cars, carOrder } from '../data/cars.js?v=20260924-r165';
 import { engines } from '../data/engines.js?v=20260924-r164';
 import { applyEngineTuning } from '../data/tuning.js?v=20260922-r114';
 import { applySecondaryTuning, getExhaustNosTuning } from '../data/secondaryTuning.js?v=20260922-r128';
