8efbcc0ba9928649aeadd044ac43dfa687a3da9a
azyuen <azyuen@gmail.com>
2026-09-23 16:39:32 +1000

    Bump character roster in saved game normalisation

--- src/state/GameState.js
+++ src/state/GameState.js
@@ -1,4 +1,4 @@
-import { playableCharacterOrder, rivalCharacterOrder } from '../data/characters.js?v=20260923-r140';
+import { playableCharacterOrder, rivalCharacterOrder } from '../data/characters.js?v=20260923-r145';
 import {
   WORKSHOP_TIERS,
   getWorkshopByLocationId,
