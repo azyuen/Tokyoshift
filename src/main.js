45bf1dcdd4671a17bd322b9942270dc4c43bda17
azyuen <azyuen@gmail.com>
2026-09-23 23:49:50 +1000

    Load final R161 scene revisions

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
 import BootScene from './scenes/BootScene.js?v=20260923-r158';
 import CharacterSelectScene from './scenes/CharacterSelectScene.js?v=20260923-r158';
-import GarageScene from './scenes/GarageScene.js?v=20260923-r160';
-import CentralTokyoScene from './scenes/CentralTokyoScene.js?v=20260923-r160';
-import MeetScene from './scenes/MeetScene.js?v=20260923-r160';
-import RaceScene from './scenes/RaceScene.js?v=20260923-r160';
+import GarageScene from './scenes/GarageScene.js?v=20260923-r161';
+import CentralTokyoScene from './scenes/CentralTokyoScene.js?v=20260923-r161';
+import MeetScene from './scenes/MeetScene.js?v=20260923-r161';
+import RaceScene from './scenes/RaceScene.js?v=20260923-r161';
 import ResultScene from './scenes/ResultScene.js?v=20260922-r112';
 
 
