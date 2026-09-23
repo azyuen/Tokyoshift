0502ee280f10daac673929284aecba5170d7cc24
azyuen <azyuen@gmail.com>
2026-09-23 20:00:31 +1000

    R151 rotate cache after Ginza wheel mirror fix

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 const BUILD = 'R149';
-const ASSET_CACHE = 'tokyoshift-assets-v95';
-const RUNTIME_CACHE = 'tokyoshift-runtime-v95';
+const ASSET_CACHE = 'tokyoshift-assets-v96';
+const RUNTIME_CACHE = 'tokyoshift-runtime-v96';
 
 self.addEventListener('install', () => self.skipWaiting());
 
