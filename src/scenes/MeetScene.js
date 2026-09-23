3e27b17b2c9a60f817ad22a6f3cf447c8f2560cc
azyuen <azyuen@gmail.com>
2026-09-23 22:28:12 +1000

    R158 load recalibrated hero assets in MeetScene.js

--- src/scenes/MeetScene.js
+++ src/scenes/MeetScene.js
@@ -1,4 +1,4 @@
-import { cars, carOrder } from '../data/cars.js?v=20260923-r155';
+import { cars, carOrder } from '../data/cars.js?v=20260923-r158';
 import {
   DEFAULT_PAINT_COLOR,
   RIVAL_PAINT_COLORS,
