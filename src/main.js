3f2fcb82626558183d8015f7b8795d1547394588
azyuen <azyuen@gmail.com>
2026-09-23 22:12:49 +1000

    Bump Tokyo SHIFT build to R157 (src/main.js)

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import BootScene from './scenes/BootScene.js?v=20260923-r155';
 import CharacterSelectScene from './scenes/CharacterSelectScene.js?v=20260923-r155';
 import GarageScene from './scenes/GarageScene.js?v=20260923-r155';
 import CentralTokyoScene from './scenes/CentralTokyoScene.js?v=20260923-r155';
-import MeetScene from './scenes/MeetScene.js?v=20260923-r156';
+import MeetScene from './scenes/MeetScene.js?v=20260923-r157';
 import RaceScene from './scenes/RaceScene.js?v=20260923-r155';
 import ResultScene from './scenes/ResultScene.js?v=20260922-r112';
 
