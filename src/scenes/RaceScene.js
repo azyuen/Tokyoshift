a94d385c8cdee0b4d509a3b0f4595f41e01c4567
azyuen <azyuen@gmail.com>
2026-09-24 08:13:19 +1000

    R164 load new hero car data in RaceScene.js

--- src/scenes/RaceScene.js
+++ src/scenes/RaceScene.js
@@ -4,7 +4,7 @@ import DragRacingAI from '../ai/DragRacingAI.js?v=20260923-r162';
 import RaceHUD from '../ui/RaceHUD.js?v=20260921-r43';
 import DebugHUD from '../ui/DebugHUD.js';
 import TokyoExpresswayBackground from '../environment/TokyoExpresswayBackground.js?v=20260921-r49';
-import { cars, carOrder } from '../data/cars.js?v=20260923-r160';
+import { cars, carOrder } from '../data/cars.js?v=20260924-r164';
 import {
   DEFAULT_PAINT_COLOR,
   getCarPaintColor,
@@ -14,7 +14,7 @@ import {
 import { createDriverSilhouette } from '../vehicles/DriverSilhouette.js?v=20260923-r137';
 import { createVisualModLayers } from '../data/visualMods.js?v=20260923-r138';
 import { getWheelPairFit, getWheelContactOffsetY } from '../vehicles/WheelFit.js?v=20260923-r160';
-import { engines } from '../data/engines.js?v=20260923-r134';
+import { engines } from '../data/engines.js?v=20260924-r164';
 import { applyEngineTuning } from '../data/tuning.js?v=20260921-r55';
 import { applySecondaryTuning, getExhaustNosTuning } from '../data/secondaryTuning.js?v=20260922-r128';
 import {
