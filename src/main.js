2b45cdc15a1ac1b067a35ab9319bbed3729b1151
azyuen <azyuen@gmail.com>
2026-09-23 18:45:17 +1000

    R148: load revised hero car fit and Ginza presentation

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
 import BootScene from './scenes/BootScene.js?v=20260923-r146';
 import CharacterSelectScene from './scenes/CharacterSelectScene.js?v=20260923-r146';
-import GarageScene from './scenes/GarageScene.js?v=20260923-r146';
-import CentralTokyoScene from './scenes/CentralTokyoScene.js?v=20260923-r147';
-import MeetScene from './scenes/MeetScene.js?v=20260923-r146';
-import RaceScene from './scenes/RaceScene.js?v=20260923-r146';
+import GarageScene from './scenes/GarageScene.js?v=20260923-r148';
+import CentralTokyoScene from './scenes/CentralTokyoScene.js?v=20260923-r148';
+import MeetScene from './scenes/MeetScene.js?v=20260923-r148';
+import RaceScene from './scenes/RaceScene.js?v=20260923-r148';
 import ResultScene from './scenes/ResultScene.js?v=20260922-r112';
 
 
