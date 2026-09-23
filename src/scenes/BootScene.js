98f6d5cd94eae8294976c3a07a6e53996e78cf7f
azyuen <azyuen@gmail.com>
2026-09-23 21:12:43 +1000

    R154 load Mine template calibration in BootScene.js

--- src/scenes/BootScene.js
+++ src/scenes/BootScene.js
@@ -1,6 +1,6 @@
 import { garageAssets } from '../data/garageAssets.js?v=20260922-r128';
-import { cars } from '../data/cars.js?v=20260923-r152';
-import { preloadCarAppearanceAssets } from '../vehicles/CarAppearance.js?v=20260923-r134';
+import { cars } from '../data/cars.js?v=20260923-r154';
+import { preloadCarAppearanceAssets } from '../vehicles/CarAppearance.js?v=20260923-r154';
 import { characters, playableCharacterOrder } from '../data/characters.js?v=20260923-r145';
 import { createDefaultGameState, readManualSave, readSessionState, applyStateToRegistry } from '../state/GameState.js?v=20260923-r145';
 import { startSceneLoading } from '../ui/LoadingScreen.js?v=20260922-r128';
@@ -36,7 +36,7 @@ export default class BootScene extends Phaser.Scene {
 
     // Standard cars load body + tintable paint + overlay. Ginza hero cars set
     // visual.singleBody and load only their finished one-off body PNG.
-    preloadCarAppearanceAssets(this, cars, '20260923-r138');
+    preloadCarAppearanceAssets(this, cars, '20260923-r154');
 
     this.load.image('wheel8Spoke', 'assets/wheels/wheel_8spoke.png');
     this.load.image('wheel5Spoke', 'assets/wheels/wheel_5spoke.png');
@@ -55,7 +55,7 @@ export default class BootScene extends Phaser.Scene {
       ) {
         this.load.image(
           visual.wheelKey,
-          visual.wheelPath + '?v=20260923-r134'
+          visual.wheelPath + '?v=20260923-r154'
         );
       }
     });
