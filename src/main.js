6d8079b1889a91c37e45d0797bf5b7358adedb7e
azyuen <azyuen@gmail.com>
2026-09-24 21:19:30 +1000

    R175 load AE86 visual modification scenes

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
-import BootScene from './scenes/BootScene.js?v=20260924-r174';
+import BootScene from './scenes/BootScene.js?v=20260924-r175';
 import CharacterSelectScene from './scenes/CharacterSelectScene.js?v=20260924-r171';
-import GarageScene from './scenes/GarageScene.js?v=20260924-r173';
-import CentralTokyoScene from './scenes/CentralTokyoScene.js?v=20260924-r171';
+import GarageScene from './scenes/GarageScene.js?v=20260924-r175';
+import CentralTokyoScene from './scenes/CentralTokyoScene.js?v=20260924-r175';
 import MeetScene from './scenes/MeetScene.js?v=20260924-r171';
-import RaceScene from './scenes/RaceScene.js?v=20260924-r173';
+import RaceScene from './scenes/RaceScene.js?v=20260924-r175';
 import ResultScene from './scenes/ResultScene.js?v=20260924-r171';
-import TunerShopScene from './scenes/TunerShopScene.js?v=20260924-r174';
+import TunerShopScene from './scenes/TunerShopScene.js?v=20260924-r175';
 
 
 // Phone readability pass.
