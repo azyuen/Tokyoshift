e8967b500d93077755a0b4dc46e854eaa85e5220
azyuen <azyuen@gmail.com>
2026-09-24 21:17:57 +1000

    R175 preload AE86 custom aero assets

--- src/scenes/BootScene.js
+++ src/scenes/BootScene.js
@@ -4,7 +4,7 @@ import { preloadCarAppearanceAssets, ensureDerivedModularCarTextures } from '../
 import { characters, playableCharacterOrder } from '../data/characters.js?v=20260923-r145';
 import { createDefaultGameState, readManualSave, readSessionState, applyStateToRegistry } from '../state/GameState.js?v=20260924-r172';
 import { startSceneLoading } from '../ui/LoadingScreen.js?v=20260922-r128';
-import { ensureVisualModTextures } from '../data/visualMods.js?v=20260924-r170';
+import { ensureVisualModTextures, preloadVisualModAssets } from '../data/visualMods.js?v=20260924-r175';
 import { TUNER_SHOPS } from '../data/tunerShops.js?v=20260924-r172';
 
 export default class BootScene extends Phaser.Scene {
@@ -38,6 +38,7 @@ export default class BootScene extends Phaser.Scene {
     // Standard cars load body + tintable paint + overlay. Ginza hero cars set
     // visual.singleBody and load only their finished one-off body PNG.
     preloadCarAppearanceAssets(this, cars, '20260924-r171');
+    preloadVisualModAssets(this, '20260924-r175');
 
     this.load.image('wheel8Spoke', 'assets/wheels/wheel_8spoke.png');
     this.load.image('wheel5Spoke', 'assets/wheels/wheel_5spoke.png');
