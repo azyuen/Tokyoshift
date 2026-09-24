0c6067a41f12c3f3356ade6d564660b4c6ee380d
azyuen <azyuen@gmail.com>
2026-09-24 22:45:52 +1000

    R177 load one-layer AE86 visual mods

--- src/scenes/RaceScene.js
+++ src/scenes/RaceScene.js
@@ -12,7 +12,7 @@ import {
   createCarBodyLayers,
 } from '../vehicles/CarAppearance.js?v=20260924-r170';
 import { createDriverSilhouette } from '../vehicles/DriverSilhouette.js?v=20260923-r137';
-import { createVisualModLayers } from '../data/visualMods.js?v=20260924-r175';
+import { createVisualModLayers } from '../data/visualMods.js?v=20260924-r177';
 import { createTunerDecalLayers } from '../vehicles/TunerDecals.js?v=20260924-r176';
 import { getWheelPairFit, getWheelContactOffsetY } from '../vehicles/WheelFit.js?v=20260923-r160';
 import { engines } from '../data/engines.js?v=20260924-r164';
