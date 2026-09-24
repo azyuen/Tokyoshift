4945181150140c22700508d686da7948141e6e6e
azyuen <azyuen@gmail.com>
2026-09-24 16:37:31 +1000

    R171 load AE86 visual calibration

--- src/scenes/BootScene.js
+++ src/scenes/BootScene.js
@@ -1,5 +1,5 @@
 import { garageAssets } from '../data/garageAssets.js?v=20260922-r128';
-import { cars } from '../data/cars.js?v=20260924-r170';
+import { cars } from '../data/cars.js?v=20260924-r171';
 import { preloadCarAppearanceAssets, ensureDerivedModularCarTextures } from '../vehicles/CarAppearance.js?v=20260924-r170';
 import { characters, playableCharacterOrder } from '../data/characters.js?v=20260923-r145';
 import { createDefaultGameState, readManualSave, readSessionState, applyStateToRegistry } from '../state/GameState.js?v=20260924-r168';
@@ -37,7 +37,7 @@ export default class BootScene extends Phaser.Scene {
 
     // Standard cars load body + tintable paint + overlay. Ginza hero cars set
     // visual.singleBody and load only their finished one-off body PNG.
-    preloadCarAppearanceAssets(this, cars, '20260924-r170');
+    preloadCarAppearanceAssets(this, cars, '20260924-r171');
 
     this.load.image('wheel8Spoke', 'assets/wheels/wheel_8spoke.png');
     this.load.image('wheel5Spoke', 'assets/wheels/wheel_5spoke.png');
