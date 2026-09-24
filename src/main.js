6f932f7b04fe6a6607078424ebbcb71831cc0fb3
azyuen <azyuen@gmail.com>
2026-09-24 18:45:08 +1000

    Load R173 tuner-shop refinements

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
-import BootScene from './scenes/BootScene.js?v=20260924-r172';
+import BootScene from './scenes/BootScene.js?v=20260924-r173';
 import CharacterSelectScene from './scenes/CharacterSelectScene.js?v=20260924-r171';
-import GarageScene from './scenes/GarageScene.js?v=20260924-r172';
+import GarageScene from './scenes/GarageScene.js?v=20260924-r173';
 import CentralTokyoScene from './scenes/CentralTokyoScene.js?v=20260924-r171';
 import MeetScene from './scenes/MeetScene.js?v=20260924-r171';
-import RaceScene from './scenes/RaceScene.js?v=20260924-r172';
+import RaceScene from './scenes/RaceScene.js?v=20260924-r173';
 import ResultScene from './scenes/ResultScene.js?v=20260924-r171';
-import TunerShopScene from './scenes/TunerShopScene.js?v=20260924-r172';
+import TunerShopScene from './scenes/TunerShopScene.js?v=20260924-r173';
 
 
 // Phone readability pass.
