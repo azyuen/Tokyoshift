8d72d8a216624b2378cd7ed5dede7423af5c36b1
azyuen <azyuen@gmail.com>
2026-09-24 15:19:14 +1000

    R170 bust scene cache for AE86 alignment fix

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
-import BootScene from './scenes/BootScene.js?v=20260924-r169';
-import CharacterSelectScene from './scenes/CharacterSelectScene.js?v=20260924-r169';
-import GarageScene from './scenes/GarageScene.js?v=20260924-r169';
-import CentralTokyoScene from './scenes/CentralTokyoScene.js?v=20260924-r169';
-import MeetScene from './scenes/MeetScene.js?v=20260924-r169';
-import RaceScene from './scenes/RaceScene.js?v=20260924-r169';
-import ResultScene from './scenes/ResultScene.js?v=20260924-r169';
-import TunerShopScene from './scenes/TunerShopScene.js?v=20260924-r169';
+import BootScene from './scenes/BootScene.js?v=20260924-r170';
+import CharacterSelectScene from './scenes/CharacterSelectScene.js?v=20260924-r170';
+import GarageScene from './scenes/GarageScene.js?v=20260924-r170';
+import CentralTokyoScene from './scenes/CentralTokyoScene.js?v=20260924-r170';
+import MeetScene from './scenes/MeetScene.js?v=20260924-r170';
+import RaceScene from './scenes/RaceScene.js?v=20260924-r170';
+import ResultScene from './scenes/ResultScene.js?v=20260924-r170';
+import TunerShopScene from './scenes/TunerShopScene.js?v=20260924-r170';
 
 
 // Phone readability pass.
