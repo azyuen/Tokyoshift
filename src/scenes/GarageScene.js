1704148007de3ee03a01e2e727c9300772c4fb46
azyuen <azyuen@gmail.com>
2026-09-24 23:02:12 +1000

    Refresh tuner challenge module revisions for R178

--- src/scenes/GarageScene.js
+++ src/scenes/GarageScene.js
@@ -32,11 +32,11 @@ import {
   getExhaustNosCartCost,
   applySecondaryTuning,
 } from '../data/secondaryTuning.js?v=20260924-r176';
-import { saveManualState, saveSessionState } from '../state/GameState.js?v=20260924-r168';
+import { saveManualState, saveSessionState } from '../state/GameState.js?v=20260924-r178';
 import { addSettingsButton, showSettingsPanel } from '../ui/SettingsPanel.js?v=20260922-r125';
 import { getMeetLocation } from '../data/meetAssets.js?v=20260922-r84';
 import { getTravelLocation } from '../data/travelRegions.js?v=20260923-r144';
-import { showTravelMap } from '../ui/TravelMap.js?v=20260924-r176';
+import { showTravelMap } from '../ui/TravelMap.js?v=20260924-r178';
 import {
   CENTRAL_TOKYO_LOCATIONS,
   getPendingCentralTokyoInvite,
