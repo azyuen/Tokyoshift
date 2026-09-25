9ba65feb0620b122cd17e2593d4fa4e5f6ac1458
azyuen <azyuen@gmail.com>
2026-09-25 21:47:34 +1000

    R196: load canonical AE86 aero renderer

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
-import BootScene from './scenes/BootScene.js?v=20260925-r195';
+import BootScene from './scenes/BootScene.js?v=20260925-r196';
 import CharacterSelectScene from './scenes/CharacterSelectScene.js?v=20260925-r195';
-import GarageScene from './scenes/GarageScene.js?v=20260925-r195';
-import CentralTokyoScene from './scenes/CentralTokyoScene.js?v=20260925-r195';
+import GarageScene from './scenes/GarageScene.js?v=20260925-r196';
+import CentralTokyoScene from './scenes/CentralTokyoScene.js?v=20260925-r196';
 import MeetScene from './scenes/MeetScene.js?v=20260925-r195';
-import RaceScene from './scenes/RaceScene.js?v=20260925-r195';
+import RaceScene from './scenes/RaceScene.js?v=20260925-r196';
 import ResultScene from './scenes/ResultScene.js?v=20260924-r171';
-import TunerShopScene from './scenes/TunerShopScene.js?v=20260925-r195';
+import TunerShopScene from './scenes/TunerShopScene.js?v=20260925-r196';
 import ProfileCalibrationScene from './scenes/ProfileCalibrationScene.js?v=20260925-r195';
 
 
