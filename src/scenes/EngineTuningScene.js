a04b3165aafee7299484ff0fbd71f7d7094d2a70
azyuen <azyuen@gmail.com>
2026-09-25 18:20:40 +1000

    R191: refresh AE86 body alignment modules

--- src/scenes/EngineTuningScene.js
+++ src/scenes/EngineTuningScene.js
@@ -1,10 +1,10 @@
-import { getCarBodyScaleForWidth } from '../vehicles/CarAppearance.js?v=20260925-r190';
-import { cars } from '../data/cars.js?v=20260925-r190';
+import { getCarBodyScaleForWidth } from '../vehicles/CarAppearance.js?v=20260925-r191';
+import { cars } from '../data/cars.js?v=20260925-r191';
 import {
   getCarBodyTextureKey,
   createCarBodyLayers,
   getCarPaintColor,
-} from '../vehicles/CarAppearance.js?v=20260925-r190';
+} from '../vehicles/CarAppearance.js?v=20260925-r191';
 import { engines } from '../data/engines.js?v=20260924-r164';
 import { getWheelPairFit } from '../vehicles/WheelFit.js?v=20260923-r152';
 import { characters } from '../data/characters.js?v=20260921-r43';
