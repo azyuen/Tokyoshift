c25d9217ab7b199161311dc1794e9aea331ad2f8
azyuen <azyuen@gmail.com>
2026-09-26 00:43:21 +1000

    R201: emergency rollback to stable AE86 rendering

--- src/scenes/RaceScene.js
+++ src/scenes/RaceScene.js
@@ -12,7 +12,7 @@ import {
   createCarBodyLayers,
 } from '../vehicles/CarAppearance.js?v=20260925-r193';
 import { createDriverSilhouette } from '../vehicles/DriverSilhouette.js?v=20260923-r137';
-import { createVisualModLayers } from '../data/visualMods.js?v=20260926-r200';
+import { createVisualModLayers } from '../data/visualMods.js?v=20260926-r201';
 import { createTunerDecalLayers } from '../vehicles/TunerDecals.js?v=20260924-r176';
 import { getWheelPairFit, getWheelContactOffsetY } from '../vehicles/WheelFit.js?v=20260923-r160';
 import { engines } from '../data/engines.js?v=20260924-r164';
