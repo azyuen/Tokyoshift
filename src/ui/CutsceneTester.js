d29dd881e22b10c865098c49548784978271a6da
azyuen <azyuen@gmail.com>
2026-09-25 14:10:44 +1000

    Refresh tester manga revision

--- src/ui/CutsceneTester.js
+++ src/ui/CutsceneTester.js
@@ -6,7 +6,7 @@ import {
 import {
   playMangaCutscene,
   sceneCutsceneActive,
-} from './MangaCutscene.js?v=20260925-r185';
+} from './MangaCutscene.js?v=20260925-r186';
 
 const PIXEL_FONT = '"Silkscreen", monospace';
 const BODY_FONT = '"Rajdhani", monospace';
