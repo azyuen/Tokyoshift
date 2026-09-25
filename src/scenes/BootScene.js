b19d594756a1947390503a1721d8ae7d79a8dc3e
azyuen <azyuen@gmail.com>
2026-09-25 23:31:05 +1000

    R198: load paired AE86 body kits

--- src/scenes/BootScene.js
+++ src/scenes/BootScene.js
@@ -4,7 +4,7 @@ import { preloadCarAppearanceAssets, ensureDerivedModularCarTextures } from '../
 import { characters, playableCharacterOrder } from '../data/characters.js?v=20260925-r195';
 import { createDefaultGameState, readManualSave, readSessionState, applyStateToRegistry } from '../state/GameState.js?v=20260925-r195';
 import { startSceneLoading } from '../ui/LoadingScreen.js?v=20260922-r128';
-import { ensureVisualModTextures, preloadVisualModAssets } from '../data/visualMods.js?v=20260925-r196';
+import { ensureVisualModTextures, preloadVisualModAssets } from '../data/visualMods.js?v=20260925-r198';
 import { TUNER_SHOPS } from '../data/tunerShops.js?v=20260924-r178';
 
 export default class BootScene extends Phaser.Scene {
@@ -38,7 +38,7 @@ export default class BootScene extends Phaser.Scene {
     // Standard cars load body + tintable paint + overlay. Ginza hero cars set
     // visual.singleBody and load only their finished one-off body PNG.
     preloadCarAppearanceAssets(this, cars, '20260925-r193');
-    preloadVisualModAssets(this, '20260925-r196');
+    preloadVisualModAssets(this, '20260925-r198');
 
     this.load.image('wheel8Spoke', 'assets/wheels/wheel_8spoke.png');
     this.load.image('wheel5Spoke', 'assets/wheels/wheel_5spoke.png');
