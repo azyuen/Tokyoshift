490d1bb0516b7af84f8feff44cddcdd169531382
azyuen <azyuen@gmail.com>
2026-09-25 18:20:51 +1000

    R191: refresh AE86 body alignment modules

--- src/scenes/MeetScene.js
+++ src/scenes/MeetScene.js
@@ -1,5 +1,5 @@
-import { getCarBodyScaleForWidth } from '../vehicles/CarAppearance.js?v=20260925-r190';
-import { cars, carOrder } from '../data/cars.js?v=20260925-r190';
+import { getCarBodyScaleForWidth } from '../vehicles/CarAppearance.js?v=20260925-r191';
+import { cars, carOrder } from '../data/cars.js?v=20260925-r191';
 import { engines } from '../data/engines.js?v=20260924-r164';
 import { applyEngineTuning } from '../data/tuning.js?v=20260922-r114';
 import { applySecondaryTuning, getExhaustNosTuning } from '../data/secondaryTuning.js?v=20260924-r176';
@@ -9,7 +9,7 @@ import {
   normalisePaintColor,
   getCarBodyTextureKey,
   createCarBodyLayers,
-} from '../vehicles/CarAppearance.js?v=20260925-r190';
+} from '../vehicles/CarAppearance.js?v=20260925-r191';
 import {
   characters,
   characterOrder,
