5bad22440e7127ab03f2eb277a6cbc60ae31b251
azyuen <azyuen@gmail.com>
2026-09-25 20:19:40 +1000

    Bump Tatsumi character roster data

--- src/scenes/BootScene.js
+++ src/scenes/BootScene.js
@@ -1,7 +1,7 @@
 import { garageAssets } from '../data/garageAssets.js?v=20260925-r192';
 import { cars } from '../data/cars.js?v=20260925-r193';
 import { preloadCarAppearanceAssets, ensureDerivedModularCarTextures } from '../vehicles/CarAppearance.js?v=20260925-r193';
-import { characters, playableCharacterOrder } from '../data/characters.js?v=20260925-r182';
+import { characters, playableCharacterOrder } from '../data/characters.js?v=20260925-r195';
 import { createDefaultGameState, readManualSave, readSessionState, applyStateToRegistry } from '../state/GameState.js?v=20260925-r184';
 import { startSceneLoading } from '../ui/LoadingScreen.js?v=20260922-r128';
 import { ensureVisualModTextures, preloadVisualModAssets } from '../data/visualMods.js?v=20260925-r194';
