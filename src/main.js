8699f5f04a2f80bae39fb4be2f011a21c9b9b366
azyuen <azyuen@gmail.com>
2026-09-26 00:43:36 +1000

    R201: emergency rollback to stable AE86 rendering

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
-import BootScene from './scenes/BootScene.js?v=20260926-r200';
+import BootScene from './scenes/BootScene.js?v=20260926-r201';
 import CharacterSelectScene from './scenes/CharacterSelectScene.js?v=20260925-r195';
-import GarageScene from './scenes/GarageScene.js?v=20260926-r200';
-import CentralTokyoScene from './scenes/CentralTokyoScene.js?v=20260926-r200';
+import GarageScene from './scenes/GarageScene.js?v=20260926-r201';
+import CentralTokyoScene from './scenes/CentralTokyoScene.js?v=20260926-r201';
 import MeetScene from './scenes/MeetScene.js?v=20260925-r195';
-import RaceScene from './scenes/RaceScene.js?v=20260926-r200';
+import RaceScene from './scenes/RaceScene.js?v=20260926-r201';
 import ResultScene from './scenes/ResultScene.js?v=20260924-r171';
-import TunerShopScene from './scenes/TunerShopScene.js?v=20260926-r200';
+import TunerShopScene from './scenes/TunerShopScene.js?v=20260926-r201';
 import ProfileCalibrationScene from './scenes/ProfileCalibrationScene.js?v=20260925-r195';
 
 
