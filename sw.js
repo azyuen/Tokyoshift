afffb1bb5290a4994aa622f37db6189c1133a05e
azyuen <azyuen@gmail.com>
2026-09-26 00:24:07 +1000

    R200: activate six AE86 master assets

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-const BUILD = 'R199';
-const ASSET_CACHE = 'tokyoshift-assets-v140';
-const RUNTIME_CACHE = 'tokyoshift-runtime-v140';
+const BUILD = 'R200';
+const ASSET_CACHE = 'tokyoshift-assets-v141';
+const RUNTIME_CACHE = 'tokyoshift-runtime-v141';
 
 self.addEventListener('install', () => self.skipWaiting());
 
