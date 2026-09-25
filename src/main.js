c3cfb0426ada0fa991a03949349d6f3e770ed8b9
azyuen <azyuen@gmail.com>
2026-09-25 19:33:26 +1000

    R194: load AE86 aero install build

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
-import BootScene from './scenes/BootScene.js?v=20260925-r193';
+import BootScene from './scenes/BootScene.js?v=20260925-r194';
 import CharacterSelectScene from './scenes/CharacterSelectScene.js?v=20260925-r193';
-import GarageScene from './scenes/GarageScene.js?v=20260925-r193';
-import CentralTokyoScene from './scenes/CentralTokyoScene.js?v=20260925-r193';
+import GarageScene from './scenes/GarageScene.js?v=20260925-r194';
+import CentralTokyoScene from './scenes/CentralTokyoScene.js?v=20260925-r194';
 import MeetScene from './scenes/MeetScene.js?v=20260925-r193';
-import RaceScene from './scenes/RaceScene.js?v=20260925-r193';
+import RaceScene from './scenes/RaceScene.js?v=20260925-r194';
 import ResultScene from './scenes/ResultScene.js?v=20260924-r171';
-import TunerShopScene from './scenes/TunerShopScene.js?v=20260925-r193';
+import TunerShopScene from './scenes/TunerShopScene.js?v=20260925-r194';
 import ProfileCalibrationScene from './scenes/ProfileCalibrationScene.js?v=20260925-r184';
 
 
