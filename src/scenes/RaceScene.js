8be2d491b8e1b5f180a71f51372cd7fecaf48d89
azyuen <azyuen@gmail.com>
2026-09-25 14:47:09 +1000

    Refresh Race cutscene tester revision

--- src/scenes/RaceScene.js
+++ src/scenes/RaceScene.js
@@ -41,7 +41,7 @@ import {
   getTunerTeamChallengeState,
 } from '../data/tunerChallenges.js?v=20260924-r178';
 import { createCharacterProfile } from '../characters/CharacterProfileRenderer.js?v=20260925-r184';
-import { addDevCutsceneButton } from '../ui/CutsceneTester.js?v=20260925-r186';
+import { addDevCutsceneButton } from '../ui/CutsceneTester.js?v=20260925-r188';
 import { playMangaCutscene, sceneCutsceneActive } from '../ui/MangaCutscene.js?v=20260925-r188';
 
 const QUARTER_M = 402.336;
