da1c32a4d4ac82ebd43969508bcb5514c696c559
azyuen <azyuen@gmail.com>
2026-09-25 19:11:22 +1000

    R193: align car module cache version

--- src/scenes/CharacterSelectScene.js
+++ src/scenes/CharacterSelectScene.js
@@ -1,10 +1,10 @@
-import { getCarBodyScaleForWidth } from '../vehicles/CarAppearance.js?v=20260925-r191';
-import { cars } from '../data/cars.js?v=20260925-r191';
+import { getCarBodyScaleForWidth } from '../vehicles/CarAppearance.js?v=20260925-r193';
+import { cars } from '../data/cars.js?v=20260925-r193';
 import {
   DEFAULT_PAINT_COLOR,
   getCarBodyTextureKey,
   createCarBodyLayers,
-} from '../vehicles/CarAppearance.js?v=20260925-r191';
+} from '../vehicles/CarAppearance.js?v=20260925-r193';
 import { getWheelPairFit } from '../vehicles/WheelFit.js?v=20260923-r160';
 import { characters, playableCharacterOrder } from '../data/characters.js?v=20260925-r182';
 import { createDefaultGameState, applyStateToRegistry, saveManualState } from '../state/GameState.js?v=20260925-r184';
