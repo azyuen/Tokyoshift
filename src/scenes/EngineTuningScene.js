1f05766b5c9489ca919ab4f3c6b255ff2567bf9e
azyuen <azyuen@gmail.com>
2026-09-25 19:11:19 +1000

    R193: align car module cache version

--- src/scenes/EngineTuningScene.js
+++ src/scenes/EngineTuningScene.js
@@ -1,10 +1,10 @@
-import { getCarBodyScaleForWidth } from '../vehicles/CarAppearance.js?v=20260925-r191';
-import { cars } from '../data/cars.js?v=20260925-r191';
+import { getCarBodyScaleForWidth } from '../vehicles/CarAppearance.js?v=20260925-r193';
+import { cars } from '../data/cars.js?v=20260925-r193';
 import {
   getCarBodyTextureKey,
   createCarBodyLayers,
   getCarPaintColor,
-} from '../vehicles/CarAppearance.js?v=20260925-r191';
+} from '../vehicles/CarAppearance.js?v=20260925-r193';
 import { engines } from '../data/engines.js?v=20260924-r164';
 import { getWheelPairFit } from '../vehicles/WheelFit.js?v=20260923-r152';
 import { characters } from '../data/characters.js?v=20260921-r43';
