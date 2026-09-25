e1862983b530d9c575f7c2b43d2804f31521b35e
azyuen <azyuen@gmail.com>
2026-09-25 19:32:26 +1000

    R194: enable AE86 aero install UI

--- src/scenes/GarageScene.js
+++ src/scenes/GarageScene.js
@@ -79,7 +79,7 @@ import {
   normaliseVisualMods,
   getVisualModChangeCost,
   createVisualModLayers,
-} from '../data/visualMods.js?v=20260925-r193';
+} from '../data/visualMods.js?v=20260925-r194';
 import { createTunerDecalLayers } from '../vehicles/TunerDecals.js?v=20260924-r176';
 import { getWheelPairFit, getWheelContactOffsetY } from '../vehicles/WheelFit.js?v=20260923-r160';
 
