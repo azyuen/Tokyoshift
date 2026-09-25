9bec0e6e81b7de0721295caf1be052c676cdf4b8
azyuen <azyuen@gmail.com>
2026-09-25 18:46:04 +1000

    R192 load exhaust NOS sprite metadata

--- src/scenes/GarageScene.js
+++ src/scenes/GarageScene.js
@@ -32,7 +32,7 @@ import {
   getChassisCartCost,
   getExhaustNosCartCost,
   applySecondaryTuning,
-} from '../data/secondaryTuning.js?v=20260924-r176';
+} from '../data/secondaryTuning.js?v=20260925-r192';
 import { saveManualState, saveSessionState } from '../state/GameState.js?v=20260925-r184';
 import { addSettingsButton, showSettingsPanel } from '../ui/SettingsPanel.js?v=20260925-r188';
 import { playMangaCutscene } from '../ui/MangaCutscene.js?v=20260925-r188';
