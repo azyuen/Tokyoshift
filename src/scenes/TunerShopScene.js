d8ed22098161ebdfed3b6b72884694ffad8da3cd
azyuen <azyuen@gmail.com>
2026-09-25 23:46:57 +1000

    R199: refresh AE86 master body rendering

--- src/scenes/TunerShopScene.js
+++ src/scenes/TunerShopScene.js
@@ -14,7 +14,7 @@ import {
   createCarBodyLayers,
   getCarPaintColor,
 } from '../vehicles/CarAppearance.js?v=20260925-r193';
-import { createVisualModLayers } from '../data/visualMods.js?v=20260925-r198';
+import { createVisualModLayers } from '../data/visualMods.js?v=20260925-r199';
 import {
   getWheelPairFit,
   getWheelContactOffsetY,
