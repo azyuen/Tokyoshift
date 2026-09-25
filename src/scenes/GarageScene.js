32ca9ef69414d34c2710665f430bcb579753fb95
azyuen <azyuen@gmail.com>
2026-09-25 20:22:37 +1000

    Refresh settings panel for Tatsumi roster

--- src/scenes/GarageScene.js
+++ src/scenes/GarageScene.js
@@ -34,7 +34,7 @@ import {
   applySecondaryTuning,
 } from '../data/secondaryTuning.js?v=20260925-r192';
 import { saveManualState, saveSessionState } from '../state/GameState.js?v=20260925-r195';
-import { addSettingsButton, showSettingsPanel } from '../ui/SettingsPanel.js?v=20260925-r188';
+import { addSettingsButton, showSettingsPanel } from '../ui/SettingsPanel.js?v=20260925-r195';
 import { playMangaCutscene } from '../ui/MangaCutscene.js?v=20260925-r195';
 import { getMeetLocation } from '../data/meetAssets.js?v=20260922-r84';
 import { getTravelLocation } from '../data/travelRegions.js?v=20260923-r144';
