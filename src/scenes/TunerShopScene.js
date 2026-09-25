b7c4a21155863b55bb2662ebd34f98a298d21952
azyuen <azyuen@gmail.com>
2026-09-25 23:31:12 +1000

    R198: load paired AE86 body kits

--- src/scenes/TunerShopScene.js
+++ src/scenes/TunerShopScene.js
@@ -14,7 +14,7 @@ import {
   createCarBodyLayers,
   getCarPaintColor,
 } from '../vehicles/CarAppearance.js?v=20260925-r193';
-import { createVisualModLayers } from '../data/visualMods.js?v=20260925-r196';
+import { createVisualModLayers } from '../data/visualMods.js?v=20260925-r198';
 import {
   getWheelPairFit,
   getWheelContactOffsetY,
