bc3f3081370a2497006464e2f8a78d4cb8f2803b
azyuen <azyuen@gmail.com>
2026-09-25 19:11:25 +1000

    R193: align car module cache version

--- src/scenes/CentralTokyoScene.js
+++ src/scenes/CentralTokyoScene.js
@@ -1,5 +1,5 @@
-import { getCarBodyScaleForWidth } from '../vehicles/CarAppearance.js?v=20260925-r191';
-import { cars, carOrder } from '../data/cars.js?v=20260925-r191';
+import { getCarBodyScaleForWidth } from '../vehicles/CarAppearance.js?v=20260925-r193';
+import { cars, carOrder } from '../data/cars.js?v=20260925-r193';
 import { engines } from '../data/engines.js?v=20260924-r164';
 import {
   characters,
@@ -17,9 +17,9 @@ import {
   getCarBodyTextureKey,
   createCarBodyLayers,
   getCarPaintColor,
-} from '../vehicles/CarAppearance.js?v=20260925-r191';
+} from '../vehicles/CarAppearance.js?v=20260925-r193';
 import { createDriverSilhouette } from '../vehicles/DriverSilhouette.js?v=20260923-r137';
-import { createVisualModLayers } from '../data/visualMods.js?v=20260925-r191';
+import { createVisualModLayers } from '../data/visualMods.js?v=20260925-r193';
 import { getWheelPairFit, getWheelContactOffsetY } from '../vehicles/WheelFit.js?v=20260923-r160';
 import { getEncounterAi } from '../data/encounterProfiles.js?v=20260921-r76';
 import { saveSessionState } from '../state/GameState.js?v=20260925-r184';
