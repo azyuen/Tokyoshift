718d87295614ace0d9ec19047ca0f04b858f0470
azyuen <azyuen@gmail.com>
2026-09-25 14:11:02 +1000

    Refresh Meet settings revision

--- src/scenes/MeetScene.js
+++ src/scenes/MeetScene.js
@@ -26,7 +26,7 @@ import {
 } from '../data/meetAssets.js?v=20260922-r84';
 import { playMusic } from '../audio/MusicManager.js?v=20260922-r99';
 import { saveSessionState } from '../state/GameState.js?v=20260925-r184';
-import { addSettingsButton } from '../ui/SettingsPanel.js?v=20260925-r185';
+import { addSettingsButton } from '../ui/SettingsPanel.js?v=20260925-r186';
 import { showTravelMap } from '../ui/TravelMap.js?v=20260924-r178';
 import { getTravelLocation } from '../data/travelRegions.js?v=20260923-r144';
 import { getGarageCapacity, getUnlockedWorkshops, getCarsInWorkshop, isWorkshopUnlocked } from '../data/workshopProgression.js?v=20260924-r163';
