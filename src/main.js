9328533fe7175b0f50039a8be5da6ffaadb1b6c8
azyuen <azyuen@gmail.com>
2026-09-25 23:31:28 +1000

    R198: refresh AE86 body kit scenes

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
-import BootScene from './scenes/BootScene.js?v=20260925-r196';
+import BootScene from './scenes/BootScene.js?v=20260925-r198';
 import CharacterSelectScene from './scenes/CharacterSelectScene.js?v=20260925-r195';
-import GarageScene from './scenes/GarageScene.js?v=20260925-r196';
-import CentralTokyoScene from './scenes/CentralTokyoScene.js?v=20260925-r196';
+import GarageScene from './scenes/GarageScene.js?v=20260925-r198';
+import CentralTokyoScene from './scenes/CentralTokyoScene.js?v=20260925-r198';
 import MeetScene from './scenes/MeetScene.js?v=20260925-r195';
-import RaceScene from './scenes/RaceScene.js?v=20260925-r196';
+import RaceScene from './scenes/RaceScene.js?v=20260925-r198';
 import ResultScene from './scenes/ResultScene.js?v=20260924-r171';
-import TunerShopScene from './scenes/TunerShopScene.js?v=20260925-r196';
+import TunerShopScene from './scenes/TunerShopScene.js?v=20260925-r198';
 import ProfileCalibrationScene from './scenes/ProfileCalibrationScene.js?v=20260925-r195';
 
 
