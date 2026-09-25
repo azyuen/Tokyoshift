bd297098e38af5107bfcf8686aa4406e21940fde
azyuen <azyuen@gmail.com>
2026-09-25 14:46:51 +1000

    Refresh global cutscene tester registry

--- src/ui/SettingsPanel.js
+++ src/ui/SettingsPanel.js
@@ -8,7 +8,7 @@ import {
   setActiveProfileIndex,
   saveSessionState,
 } from '../state/GameState.js?v=20260925-r184';
-import { addDevCutsceneButton } from './CutsceneTester.js?v=20260925-r186';
+import { addDevCutsceneButton } from './CutsceneTester.js?v=20260925-r188';
 
 const PIXEL_FONT = '"Silkscreen", monospace';
 const BODY_FONT = '"Rajdhani", monospace';
