bd53e291c50c5ad377c3484ba7eacf4c0257ff8f
azyuen <azyuen@gmail.com>
2026-09-25 20:21:40 +1000

    Refresh Tatsumi roster dependencies in race

--- src/scenes/RaceScene.js
+++ src/scenes/RaceScene.js
@@ -26,7 +26,7 @@ import {
   hasRegionalTeam,
 } from '../data/characters.js?v=20260925-r195';
 import { WORKSHOP_RETURN_COST } from '../data/meetAssets.js?v=20260922-r84';
-import { saveSessionState, saveManualState, restoreManualSave, readManualSave, clearAllSaves } from '../state/GameState.js?v=20260925-r184';
+import { saveSessionState, saveManualState, restoreManualSave, readManualSave, clearAllSaves } from '../state/GameState.js?v=20260925-r195';
 import { playRaceMusic, playVictorySting, stopMusic } from '../audio/MusicManager.js?v=20260922-r99';
 import EngineAudioSystem from '../audio/EngineAudioSystem.js?v=20260921-r81';
 import { startSceneLoading, finishSceneLoading } from '../ui/LoadingScreen.js?v=20260922-r117';
@@ -39,10 +39,10 @@ import {
   TUNER_TEAM_CHALLENGE_STAGES,
   TUNER_TEAM_PERFECT_REWARD,
   getTunerTeamChallengeState,
-} from '../data/tunerChallenges.js?v=20260924-r178';
-import { createCharacterProfile } from '../characters/CharacterProfileRenderer.js?v=20260925-r184';
+} from '../data/tunerChallenges.js?v=20260925-r195';
+import { createCharacterProfile } from '../characters/CharacterProfileRenderer.js?v=20260925-r195';
 import { addDevCutsceneButton } from '../ui/CutsceneTester.js?v=20260925-r188';
-import { playMangaCutscene, sceneCutsceneActive } from '../ui/MangaCutscene.js?v=20260925-r188';
+import { playMangaCutscene, sceneCutsceneActive } from '../ui/MangaCutscene.js?v=20260925-r195';
 
 const QUARTER_M = 402.336;
 const HALF_MILE_M = 804.672;
