bc5c87c148234f171ae47dec7f37370d2230dd1d
azyuen <azyuen@gmail.com>
2026-09-26 20:22:48 +1000

    Bump Daikoku character data dependencies to R213

--- src/scenes/BootScene.js
+++ src/scenes/BootScene.js
@@ -1,14 +1,14 @@
 import { garageAssets } from '../data/garageAssets.js?v=20260925-r192';
 import { cars, carOrder } from '../data/cars.js?v=20260926-r209';
 import { preloadCarAppearanceAssets, preloadCarWheel, ensureDerivedModularCarTextures } from '../vehicles/CarAppearance.js?v=20260926-r202';
-import { characters } from '../data/characters.js?v=20260925-r195';
+import { characters } from '../data/characters.js?v=20260926-r213';
 import {
   createDefaultGameState,
   readManualSave,
   readSessionState,
   applyStateToRegistry,
   getProfileSlots,
-} from '../state/GameState.js?v=20260926-r209';
+} from '../state/GameState.js?v=20260926-r213';
 import { startSceneLoading } from '../ui/LoadingScreen.js?v=20260922-r128';
 import { ensureVisualModTextures, preloadVisualModAssets } from '../data/visualMods.js?v=20260926-r209';
 import { TUNER_SHOPS } from '../data/tunerShops.js?v=20260924-r178';
