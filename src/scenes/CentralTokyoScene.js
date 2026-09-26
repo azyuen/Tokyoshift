6c17b80ca0bc17fae550b8d60873e434f27d5a22
azyuen <azyuen@gmail.com>
2026-09-26 12:20:25 +1000

    R209 refresh AE86 asset module imports

--- src/scenes/CentralTokyoScene.js
+++ src/scenes/CentralTokyoScene.js
@@ -1,5 +1,5 @@
 import { getCarBodyScaleForWidth } from '../vehicles/CarAppearance.js?v=20260925-r193';
-import { cars, carOrder } from '../data/cars.js?v=20260925-r193';
+import { cars, carOrder } from '../data/cars.js?v=20260926-r209';
 import { engines } from '../data/engines.js?v=20260924-r164';
 import {
   characters,
@@ -19,7 +19,7 @@ import {
   getCarPaintColor,
 } from '../vehicles/CarAppearance.js?v=20260925-r193';
 import { createDriverSilhouette } from '../vehicles/DriverSilhouette.js?v=20260923-r137';
-import { createVisualModLayers } from '../data/visualMods.js?v=20260926-r201';
+import { createVisualModLayers } from '../data/visualMods.js?v=20260926-r209';
 import { getWheelPairFit, getWheelContactOffsetY } from '../vehicles/WheelFit.js?v=20260923-r160';
 import { getEncounterAi } from '../data/encounterProfiles.js?v=20260921-r76';
 import { saveSessionState } from '../state/GameState.js?v=20260926-r204';
